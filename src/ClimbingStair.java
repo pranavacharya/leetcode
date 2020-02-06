b24de415bbcd6cbeec2e1bff93be991365b92b2a
Pranav Acharya <apranav.acharya@gmail.com>
2020-02-06 22:27:12 +0530

    Fibonacci series

--- src/ClimbingStair.java
+++ src/ClimbingStair.java
@@ -1,26 +1,18 @@
 
 public class ClimbingStair {
 
-    public static int stair(int soFar, int target, int[] memory) {
-        if (soFar == target) {
+    public int climbStairs(int n) {
+        if (n == 1) {
             return 1;
         }
-        if (soFar > target) {
-            return 0;
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
         }
-        if (memory[soFar] > 0) {
-            return memory[soFar];
-        }
-        
-        memory[soFar] = stair(soFar + 1, target, memory) + stair(soFar + 2, target, memory);
-        
-        return memory[soFar];
-    }
-
-    public int climbStairs(int n) {
-        int[] memory = new int[n];
-        int count = stair(0, n, memory);
-        return count;
+        return second;
     }
 
     public static void main(String args[]) {
