50818852910abb0f140faecc2c133b9676d88f43
Pranav Acharya <apranav.acharya@gmail.com>
2020-02-08 22:35:30 +0530

    O of N

--- src/RotateFunction.java
+++ src/RotateFunction.java
@@ -2,22 +2,23 @@
 public class RotateFunction {
 
     public int maxRotateFunction(int[] A) {
-        if (A.length == 0) {
-            return 0;
+        int allSum = 0;
+        int len = A.length;
+        int F = 0;
+        for (int i = 0; i < len; i++) {
+            F += i * A[i];
+            allSum += A[i];
         }
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < A.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < A.length; j++) {
-                sum = sum + (A[((i + j) % A.length)] * j);
-            }
-            max = sum > max ? sum : max;
+        int max = F;
+        for (int i = len - 1; i >= 1; i--) {
+            F = F + allSum - len * A[i];
+            max = Math.max(F, max);
         }
         return max;
     }
 
     public static void main(String args[]) {
-        int[] input = {-1, -1};
+        int[] input = {2, 6, 5, 4, -1};
         RotateFunction r = new RotateFunction();
         System.out.println(r.maxRotateFunction(input));
     }
