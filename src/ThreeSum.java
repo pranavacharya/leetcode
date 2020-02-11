b1b9ca7f35c4f2acbd9fb86ac114fcea6f3b68bc
Pranav Acharya <apranav.acharya@gmail.com>
2020-02-12 01:13:34 +0530

    subset with duplicate elements

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,24 +1,51 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreeSum {
-    
-    public void subset(String soFar, String rem) {
-        if (soFar.length() == 3) {
-            char[] nums = soFar.toCharArray();
+
+    private List<List<Integer>> output;
+
+    public ThreeSum() {
+        this.output = new ArrayList<>();
+    }
+
+    public void subset(ArrayList<Integer> soFar, ArrayList<Integer> rem) {
+        if (soFar.size() == 3) {
             int sum = 0;
-            for (char i : nums) {
-                sum += Character.getNumericValue(i);
+            for (Integer i : soFar) {
+                sum += (int) i;
             }
-            if (sum == 6) {
-                System.out.println(soFar + "," + sum);
+            if (sum == 0) {
+                output.add(soFar);
             }
         } else if (!rem.isEmpty()) {
-            subset(soFar + rem.substring(0, 1), rem.substring(1));
-            subset(soFar, rem.substring(1));
+            ArrayList<Integer> leftwith;
+            ArrayList<Integer> leftwithout;
+            ArrayList<Integer> right;
+            leftwith = (ArrayList<Integer>) soFar.clone();
+            leftwith.add(rem.get(0));
+            leftwithout = (ArrayList<Integer>) soFar.clone();
+            right = (ArrayList<Integer>) rem.clone();
+            right.remove(0);
+            subset(leftwith, right);
+            subset(leftwithout, right);
         }
     }
-    
-    public static void main(String args[]) {
+
+    public List<List<Integer>> threeSum(int[] nums) {
         ThreeSum ts = new ThreeSum();
-        ts.subset("", "1234");
+        ArrayList inputlist = new ArrayList<>();
+        for (int in : nums) {
+            inputlist.add(in);
+        }
+        ts.subset(new ArrayList<>(), inputlist);
+        return ts.output;
+    }
+
+    public static void main(String args[]) {
+        int[] input = {-1, 0, 1, 2, -1, -4};
+        ThreeSum ts = new ThreeSum();
+        System.out.println(ts.threeSum(input));
     }
 }
