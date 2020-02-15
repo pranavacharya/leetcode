f6b701a36c399ee3cbf275f2c74920fabb81ffed
Pranav Acharya <apranav.acharya@gmail.com>
2020-02-15 23:22:42 +0530

    faster solution

--- src/MonotonicArray.java
+++ src/MonotonicArray.java
@@ -2,28 +2,22 @@
 public class MonotonicArray {
 
     public boolean isMonotonic(int[] A) {
-        if (A.length == 0 || A.length == 1) {
-            return true;
-        }
-        int last = A[0];
-        int sum = 0;
-        int same = 0;
-        for (int i = 1; i < A.length; i++) {
-            if (A[i] > last) {
-                sum++;
-            } else if (A[i] == last) {
-                same++;
-            } else {
-                sum--;
+        boolean increasing = true;
+        boolean decreasing = true;
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] > A[i + 1]) {
+                increasing = false;
             }
-            last = A[i];
+            if (A[i] < A[i + 1]) {
+                decreasing = false;
+            }
         }
-        return (Math.abs(sum) == A.length - 1 - same);
+        return increasing || decreasing;
     }
 
     public static void main(String args[]) {
         MonotonicArray ma = new MonotonicArray();
-        int[] A = {1, 2, 3, 4};
+        int[] A = {4, 3, 2, 2, 1};
         System.out.println(ma.isMonotonic(A));
     }
 }
