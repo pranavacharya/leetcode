bdc5cf1987ec8e3bbcf61de943f14ebc3e795803
Pranav Acharya <apranav.acharya@gmail.com>
2020-02-20 22:57:11 +0530

    0 ms solution

--- src/NextGreaterElement1.java
+++ src/NextGreaterElement1.java
@@ -1,26 +1,27 @@
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Stack;
 
 public class NextGreaterElement1 {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] map = new int[10001];
-        int[] res = new int[nums1.length];
-
-        for (int i = 0; i < nums2.length; i++) {
-            map[nums2[i]] = i;
-        }
-
+        int[] ind = new int[10000];
+        int[] max = new int[nums2.length];
+        int pointer = 0;
+        
         for (int i = 0; i < nums1.length; i++) {
-            res[i] = -1;
-            for (int j = map[nums1[i]] + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    res[i] = nums2[j];
-                    break;
-                }
+            ind[nums1[i]] = i + 1;
+        }
+        
+        int[] res = new int[nums1.length];
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
+            while (pointer > 0 && max[pointer - 1] < num) {
+                pointer--;
             }
+            if (ind[num] > 0) {
+                res[ind[num] - 1] = pointer == 0 ? -1 : max[pointer - 1];
+            }
+            max[pointer++] = num;
         }
         return res;
     }
