96ee0e25d07a0cdc481a27763b271ba6eb4a9931
Pranav Acharya <apranav.acharya@gmail.com>
2020-03-09 22:12:26 +0530

    two pointer from center

--- src/LongestPalindromeSubstring.java
+++ src/LongestPalindromeSubstring.java
@@ -2,26 +2,47 @@
 public class LongestPalindromeSubstring {
 
     public String longestPalindrome(String s) {
-
         String max = "";
         for (int i = 0; i <= s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                if (isPalindrome(s.substring(i, j))) {
-                    max = max.length() > j - i ? max : s.substring(i, j);
-                }
-            }
+            String temp, temp1, temp2;
+            temp1 = moveFromCenter(s, i);
+            temp2 = moveFromCenter2(s, i);
+            temp = temp1.length() > temp2.length() ? temp1 : temp2;
+            max = max.length() > temp.length() ? max : temp;
         }
         return max;
     }
 
-    public boolean isPalindrome(String input) {
-        char[] arr = input.toCharArray();
-        for (int i = 0; i < arr.length / 2; i++) {
-            if (arr[i] != arr[arr.length - i - 1]) {
-                return false;
+    public String moveFromCenter(String ip, int k) {
+        String ans = "";
+        int left = k;
+        int right = k;
+        while (left >= 0 && right < ip.length()) {
+            if (ip.charAt(left) == ip.charAt(right)) {
+                ans = ip.substring(left, right + 1);
+                left--;
+                right++;
+            } else {
+                break;
             }
         }
-        return true;
+        return ans;
+    }
+
+    public String moveFromCenter2(String ip, int k) {
+        String ans = "";
+        int left = k;
+        int right = k + 1;
+        while (left >= 0 && right < ip.length()) {
+            if (ip.charAt(left) == ip.charAt(right)) {
+                ans = ip.substring(left, right + 1);
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+        return ans;
     }
 
     public static void main(String args[]) {
