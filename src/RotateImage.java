8236a34a155cc9c88a7116c9c3d183425b5c1023
Pranav Acharya <apranav.acharya@gmail.com>
2020-03-19 23:04:05 +0530

    matrix transpose and then reverse

---
+++ src/RotateImage.java
@@ -0,0 +1,33 @@
+
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
+                matrix[i][matrix[i].length - 1 - j] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(" " + matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        RotateImage ri = new RotateImage();
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        ri.rotate(matrix);
+    }
+}
