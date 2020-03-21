14f8bc6cea9b6df7b3c599d8d132d7aa3ffe09fe
Pranav Acharya <apranav.acharya@gmail.com>
2020-03-21 23:24:34 +0530

    using row which are present

--- src/CinemaSeatAllocation.java
+++ src/CinemaSeatAllocation.java
@@ -1,40 +1,58 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class CinemaSeatAllocation {
 
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        int max = 0;
-        int[] row = new int[11];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(row, 0);
-            for (int[] j : reservedSeats) {
-                if (j[0] == i) {
-                    row[j[1]] = 1;
-                }
+        int max = 2 * n;
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        for (int[] j : reservedSeats) {
+            if (map.containsKey(j[0])) {
+                ArrayList list = new ArrayList(map.get(j[0]));
+                list.add(j[1]);
+                map.put(j[0], new ArrayList(list));
+            } else {
+                ArrayList list = new ArrayList();
+                list.add(j[1]);
+                map.put(j[0], new ArrayList(list));
             }
-            boolean status25 = true;
+        }
+        int[] row = new int[11];
+        for (int i : map.keySet()) {
+            Arrays.fill(row, 0);
+            ArrayList list = map.get(i);
+            while (!list.isEmpty()) {
+                int j = (int) list.remove(0);
+                row[j] = 1;
+            }
+            boolean reserved25 = false;
             for (int j = 2; j <= 5; j++) {
                 if (row[j] == 1) {
-                    status25 = false;
+                    reserved25 = true;
                 }
             }
-            boolean status47 = true;
+            boolean reserved47 = false;
             for (int j = 4; j <= 7; j++) {
                 if (row[j] == 1) {
-                    status47 = false;
+                    reserved47 = true;
                 }
             }
-            boolean status69 = true;
+            boolean reserved69 = false;
             for (int j = 6; j <= 9; j++) {
                 if (row[j] == 1) {
-                    status69 = false;
+                    reserved69 = true;
                 }
             }
-            if (status25 && status69) {
-                max = max + 2;
-            } else if (status25 || status47 || status69) {
-                max++;
+            if (reserved25 && reserved69 && reserved47) {
+                max = max - 2;
+            } else if (reserved25 && reserved47) {
+                max--;
+            } else if (reserved69 && reserved47) {
+                max--;
+            } else if (reserved25 || reserved69 || reserved47) {
+                max--;
             }
         }
         return max;
