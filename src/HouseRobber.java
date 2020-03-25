37957d24eaa313bacb290a235655f8ebef28cb32
Pranav Acharya <apranav.acharya@gmail.com>
2020-03-26 02:39:53 +0530

    dynamic programming

--- src/HouseRobber.java
+++ src/HouseRobber.java
@@ -13,12 +13,11 @@ public class HouseRobber {
         }
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        dp[1] = nums[1];
-        dp[2] = nums[2] + nums[0];
-        for (int i = 3; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 3] + nums[i], dp[i - 2] + nums[i]);
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-        return Math.max(dp[dp.length - 1], dp[dp.length - 2]);
+        return dp[dp.length - 1];
     }
 
     public static void main(String args[]) {
