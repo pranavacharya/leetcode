8c42d4a481ff70d857debe33e5bf78c06028a351
Pranav Acharya <apranav.acharya@gmail.com>
2020-03-29 16:10:47 +0530

    optimal search, modifying row and column

--- src/SearchA2DMatrix2.java
+++ src/SearchA2DMatrix2.java
@@ -5,20 +5,16 @@ public class SearchA2DMatrix2 {
         if (matrix.length == 0) {
             return false;
         }
-        int rightbound = matrix[0].length - 1;
-        for (int i = 0; i < matrix.length; i++) {
-            int low = 0;
-            int high = rightbound;
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
-                if (matrix[i][mid] == target) {
-                    return true;
-                } else if (matrix[i][mid] < target) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                    rightbound = Math.min(rightbound, mid);
-                }
+        int column = matrix[0].length - 1;
+        int row = 0;
+
+        while (row >= 0 && row < matrix.length && column >= 0 && column < matrix[row].length) {
+            if (matrix[row][column] == target) {
+                return true;
+            } else if (matrix[row][column] < target) {
+                row++;
+            } else {
+                column--;
             }
         }
         return false;
@@ -28,6 +24,6 @@ public class SearchA2DMatrix2 {
         SearchA2DMatrix2 s2m = new SearchA2DMatrix2();
         int[][] matrix = new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
         {18, 21, 23, 26, 30}};
-        System.out.println(s2m.searchMatrix(matrix, 22));
+        System.out.println(s2m.searchMatrix(matrix, 30));
     }
 }
