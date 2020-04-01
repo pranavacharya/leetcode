ebf8c69e67703b2246d2b9e57059b8f7d4c53898
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-01 18:09:50 +0530

    bit manipulation

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -1,16 +1,12 @@
 
-import java.util.Arrays;
-
 public class SingleNumber {
 
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
-                return nums[i];
-            }
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
         }
-        return nums[nums.length - 1];
+        return ans;
     }
 
     public static void main(String args[]) {
