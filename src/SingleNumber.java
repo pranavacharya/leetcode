9714b17ff016f925b25f9b9b3049cbf21c8a741d
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-01 17:55:49 +0530

    sorting the array

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -1,19 +1,16 @@
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class SingleNumber {
 
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        for (int key : map.keySet()) {
-            if (map.get(key) == 1) {
-                return key;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
             }
         }
-        return -1;
+        return nums[nums.length - 1];
     }
 
     public static void main(String args[]) {
