dac8b24a9dcd4f02c674850a483f5560c0c73610
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-04 19:15:55 +0530

    two pointers

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -4,24 +4,22 @@ import java.util.Arrays;
 public class MoveZeroes {
 
     public void moveZeroes(int[] nums) {
-        int size = nums.length;
-        for (int i = 0; i < size;) {
-            if (nums[i] == 0) {
-                for (int j = i; j < size - 1; j++) {
-                    nums[j] = nums[j + 1];
-                }
-                nums[size - 1] = 0;
-                size--;
-            } else {
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 i++;
             }
+            j++;
         }
         System.out.println(Arrays.toString(nums));
     }
 
     public static void main(String args[]) {
         MoveZeroes mz = new MoveZeroes();
-        int[] nums = new int[]{0, 0, 1};
+        int[] nums = new int[]{0, 1, 0, 3, 12};
         mz.moveZeroes(nums);
     }
 }
