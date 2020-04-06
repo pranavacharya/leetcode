9d7bf536d521f6242d61725efcca51f8106c1511
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-07 00:11:40 +0530

    using inbuilt function is alphanumeric

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -6,10 +6,10 @@ public class ValidPalindrome {
         int i = 0;
         int j = arr.length - 1;
         while (i < j) {
-            while (i < j && !((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= '0' && arr[i] <= '9'))) {
+            while (i < j && !Character.isLetterOrDigit(arr[i])) {
                 i++;
             }
-            while (i < j && !((arr[j] >= 'A' && arr[j] <= 'Z') || (arr[j] >= 'a' && arr[j] <= 'z') || (arr[j] >= '0' && arr[j] <= '9'))) {
+            while (i < j && !Character.isLetterOrDigit(arr[j])) {
                 j--;
             }
             if (Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) {
