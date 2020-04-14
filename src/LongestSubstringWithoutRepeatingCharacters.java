2f416fc56826d7e078a5ee1e11a2928e64a5e7c7
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-15 01:44:31 +0530

    sliding window

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,21 +1,22 @@
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        HashSet<Character> seen = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            int j = i;
-            while (j < s.length() && !seen.contains(s.charAt(j))) {
-                seen.add(s.charAt(j));
-                j++;
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
             }
-            max = Math.max(max, j - i);
-            seen.clear();
         }
-        return max;
+        return ans;
     }
 
     public static void main(String args[]) {
