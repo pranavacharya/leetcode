538c54409e9b7701c8ecde39d1f4020dbd2d6242
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-16 20:24:21 +0530

    prefix and suffix product

--- src/ProductofArrayExceptSelf.java
+++ src/ProductofArrayExceptSelf.java
@@ -5,29 +5,26 @@ public class ProductofArrayExceptSelf {
 
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
-        int prod = 1;
-        for (int i : nums) {
-            prod *= i;
+        int[] prefix = new int[nums.length + 1];
+        prefix[0] = 1;
+        for (int i = 1; i <= nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        int[] sufix = new int[nums.length + 1];
+        sufix[nums.length] = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            sufix[i] = sufix[i + 1] * nums[i];
         }
         for (int i = 0; i < ans.length; i++) {
-            if (nums[i] != 0) {
-                ans[i] = prod / nums[i];
-            } else {
-                int temp = 1;
-                for (int j = 0; j < nums.length; j++) {
-                    if (j != i) {
-                        temp *= nums[j];
-                    }
-                }
-                ans[i] = temp;
-            }
+            ans[i] = prefix[i] * sufix[i + 1];
         }
         return ans;
     }
 
     public static void main(String args[]) {
         ProductofArrayExceptSelf paes = new ProductofArrayExceptSelf();
-        int[] nums = new int[]{0, 0};
+        int[] nums = new int[]{1, 2, 3, 4};
         System.out.println(Arrays.toString(paes.productExceptSelf(nums)));
     }
 }
