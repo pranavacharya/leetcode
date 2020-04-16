9502e2e10f5f4f111736604e452c401e6a561273
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-16 21:55:27 +0530

    greedy approach

---
+++ src/ValidParenthesisString.java
@@ -0,0 +1,21 @@
+
+public class ValidParenthesisString {
+
+    public boolean checkValidString(String s) {
+        int lo = 0, hi = 0;
+        for (char c : s.toCharArray()) {
+            lo += c == '(' ? 1 : -1;
+            hi += c != ')' ? 1 : -1;
+            if (hi < 0) {
+                break;
+            }
+            lo = Math.max(lo, 0);
+        }
+        return lo == 0;
+    }
+
+    public static void main(String args[]) {
+        ValidParenthesisString vps = new ValidParenthesisString();
+        System.out.println(vps.checkValidString("(*)"));
+    }
+}
