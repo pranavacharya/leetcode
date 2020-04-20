a90d870bcadeb6cb300ed929e13d0d24d559e7ed
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-20 14:51:07 +0530

    recursive dfs, lower and upper bound

--- src/ValidateBinarySearchTree.java
+++ src/ValidateBinarySearchTree.java
@@ -2,22 +2,22 @@
 public class ValidateBinarySearchTree {
 
     public boolean isValidBST(TreeNode root) {
-        return dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return dfs(root, null, null);
     }
 
-    public boolean dfs(TreeNode root, int smallest, int largest) {
+    public boolean dfs(TreeNode root, Integer llimit, Integer ulimit) {
         if (root == null) {
             return true;
         }
-        if (root.left != null && (root.left.val >= root.val || root.left.val <= smallest)) {
+        if ((root.left != null && root.left.val >= root.val) || (root.left != null && llimit != null && root.left.val <= llimit)) {
             return false;
         }
-        if (root.right != null && (root.right.val <= root.val || root.right.val >= largest)) {
+        if ((root.right != null && root.right.val <= root.val) || (root.right != null && ulimit != null && root.right.val >= ulimit)) {
             return false;
         }
         boolean isValid;
-        isValid = dfs(root.left, smallest, root.val);
-        isValid = isValid && dfs(root.right, root.val, largest);
+        isValid = dfs(root.left, llimit, root.val);
+        isValid = isValid && dfs(root.right, root.val, ulimit);
         return isValid;
     }
 
