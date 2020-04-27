09a460cb915b54d069400af1ee3bfe55fe1d7dfd
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-28 02:54:16 +0530

    backtracking, local max variable

--- src/PathwithMaximumGold.java
+++ src/PathwithMaximumGold.java
@@ -1,42 +1,40 @@
 
 public class PathwithMaximumGold {
 
-    private int max;
-
-    public PathwithMaximumGold() {
-        this.max = 0;
-    }
-
     public int getMaximumGold(int[][] grid) {
+        int max = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] != 0) {
-                    explorePath(grid, i, j, 0);
+                    int local = explorePath(grid, i, j, 0, 0);
+                    max = Math.max(max, local);
                 }
             }
         }
-        return this.max;
+        return max;
     }
 
-    public void explorePath(int[][] grid, int i, int j, int sum) {
+    public int explorePath(int[][] grid, int i, int j, int sum, int max) {
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] < 0) {
-            return;
+            return max;
         } else if (grid[i][j] == 0) {
-            this.max = Math.max(this.max, sum);
+            max = Math.max(max, sum);
+            return max;
         } else {
             int gold = grid[i][j];
             sum += gold;
             grid[i][j] = -1;
             //top
-            explorePath(grid, i - 1, j, sum);
+            max = explorePath(grid, i - 1, j, sum, max);
             //bottom
-            explorePath(grid, i + 1, j, sum);
+            max = explorePath(grid, i + 1, j, sum, max);
             //left
-            explorePath(grid, i, j - 1, sum);
+            max = explorePath(grid, i, j - 1, sum, max);
             //right
-            explorePath(grid, i, j + 1, sum);
+            max = explorePath(grid, i, j + 1, sum, max);
 
             grid[i][j] = gold;
+            return max;
         }
     }
 
