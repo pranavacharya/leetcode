a70d522918d1766a423887068d79f3a9629fcd65
Pranav Acharya <apranav.acharya@gmail.com>
2020-04-30 00:27:06 +0530

    subsets recursive dfs, different approach

--- src/Subsets2.java
+++ src/Subsets2.java
@@ -1,35 +1,32 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subsets2 {
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList();
+        Arrays.sort(nums);
         subsets(nums, 0, new ArrayList(), result);
         return result;
     }
 
     public void subsets(int[] nums, int index, ArrayList path, List<List<Integer>> result) {
-        if (index == nums.length) {
-            ArrayList temp = new ArrayList(path);
-            Collections.sort(temp);
-            if (!result.contains(temp)) {
-                result.add(temp);
+        result.add(new ArrayList(path));
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
             }
-        } else {
-            for (int i = index; i < nums.length; i++) {
-                path.add(nums[i]);
-                subsets(nums, i + 1, path, result);
-                path.remove(path.size() - 1);
-                subsets(nums, i + 1, path, result);
-            }
+            path.add(nums[i]);
+            subsets(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
         }
     }
 
     public static void main(String args[]) {
         Subsets2 s2 = new Subsets2();
+//        int[] nums = new int[]{4, 4, 4, 1, 4};
         int[] nums = new int[]{1, 2, 2};
         System.out.println(s2.subsetsWithDup(nums));
     }
