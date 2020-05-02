fcc208b88bac92d68f851cdd94997d7404973ddc
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-02 21:57:23 +0530

    sort and compare

--- src/CheckIfaStringCanBreakAnotherString.java
+++ src/CheckIfaStringCanBreakAnotherString.java
@@ -1,64 +1,22 @@
 
-import java.util.HashSet;
+import java.util.Arrays;
 
 public class CheckIfaStringCanBreakAnotherString {
 
-    private HashSet<String> permutation;
-
-    public CheckIfaStringCanBreakAnotherString() {
-        permutation = new HashSet();
-    }
-
     public boolean checkIfCanBreak(String s1, String s2) {
-        permute(s1, "");
-        return permuteAndCheck(s2, "");
-    }
-
-    public void permute(String rem, String soFar) {
-        if (rem.length() == 0) {
-            this.permutation.add(soFar);
-        } else {
-            for (int i = 0; i < rem.length(); i++) {
-                char letter = rem.charAt(i);
-                String before = rem.substring(0, i);
-                String after = rem.substring(i + 1);
-                permute(before.concat(after), soFar.concat("" + letter));
-            }
-        }
-    }
-
-    public boolean permuteAndCheck(String rem, String soFar) {
-        if (rem.length() == 0) {
-            for (String key : permutation) {
-                if (checkIfBreak(key, soFar)) {
-                    System.out.println(soFar);
-                    System.out.println(key);
-                    return true;
-                }
-            }
-            return false;
-        } else {
-            boolean status = false;
-            for (int i = 0; i < rem.length(); i++) {
-                char letter = rem.charAt(i);
-                String before = rem.substring(0, i);
-                String after = rem.substring(i + 1);
-                status = status || permuteAndCheck(before.concat(after), soFar.concat("" + letter));
-            }
-            return status;
-        }
-    }
-
-    public boolean checkIfBreak(String s1, String s2) {
+        char[] s1arr = s1.toCharArray();
+        char[] s2arr = s2.toCharArray();
+        Arrays.sort(s1arr);
+        Arrays.sort(s2arr);
         boolean b1 = true, b2 = true;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) < s2.charAt(i)) {
+        for (int i = 0; i < s1arr.length; i++) {
+            if (s1arr[i] < s2arr[i]) {
                 b1 = false;
                 break;
             }
         }
-        for (int i = 0; i < s2.length(); i++) {
-            if (s2.charAt(i) < s1.charAt(i)) {
+        for (int i = 0; i < s2arr.length; i++) {
+            if (s2arr[i] < s1arr[i]) {
                 b2 = false;
                 break;
             }
