9f245946a998f6f8ec6b4b9aea60d0bc8a21d5c2
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-03 02:08:53 +0530

    short-circuit backtracking, choose element if it satisfies the condition

--- src/BeautifulArrangement.java
+++ src/BeautifulArrangement.java
@@ -10,41 +10,32 @@ public class BeautifulArrangement {
     }
 
     public int countArrangement(int N) {
-        backtracking(N, new ArrayList(), new boolean[N + 1]);
+        backtracking(N, 1, new ArrayList(), new boolean[N + 1]);
         return this.count;
     }
 
-    public void backtracking(int n, ArrayList<Integer> path, boolean[] used) {
-        if (n == path.size()) {
-            if (isBeautifulArrangement(path)) {
-                System.out.println(path);
-                this.count++;
-            }
+    public void backtracking(int n, int pos, ArrayList<Integer> path, boolean[] used) {
+        if (pos > n) {
+            //System.out.println(path);
+            this.count++;
         } else {
             for (int i = 1; i <= n; i++) {
                 if (used[i]) {
                     continue;
                 }
-                path.add(i);
-                used[i] = true;
-                backtracking(n, path, used);
-                used[i] = false;
-                path.remove(path.size() - 1);
+                if ((pos % i == 0 || i % pos == 0)) {
+                    path.add(i);
+                    used[i] = true;
+                    backtracking(n, pos + 1, path, used);
+                    used[i] = false;
+                    path.remove(path.size() - 1);
+                }
             }
         }
-    }
-
-    public boolean isBeautifulArrangement(ArrayList<Integer> path) {
-        for (int i = 0; i < path.size(); i++) {
-            if (path.get(i) % (i + 1) != 0 && (i + 1) % path.get(i) != 0) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public static void main(String args[]) {
         BeautifulArrangement ba = new BeautifulArrangement();
-        System.out.println(ba.countArrangement(2));
+        System.out.println(ba.countArrangement(14));
     }
 }
