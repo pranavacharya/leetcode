7a0fd67758d93fbd08d5378c174f026d9e3f936c
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-05 21:57:22 +0530

    math operations

--- src/Maximum69Number.java
+++ src/Maximum69Number.java
@@ -1,22 +1,30 @@
 
+import java.util.ArrayList;
+
 public class Maximum69Number {
 
     public int maximum69Number(int num) {
-        String n = Integer.toString(num);
-        char[] narr = n.toCharArray();
-        int index = 0;
-        while (index < narr.length) {
-            if (narr[index] < '9') {
-                narr[index] = '9';
+        ArrayList<Integer> digits = new ArrayList();
+        while (num > 0) {
+            digits.add(num % 10);
+            num /= 10;
+        }
+        System.out.println(digits);
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits.get(i) == 6) {
+                digits.set(i, 9);
                 break;
             }
-            index++;
         }
-        return Integer.parseInt(new String(narr));
+        int ans = 0;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            ans = ans * 10 + digits.get(i);
+        }
+        return ans;
     }
 
     public static void main(String args[]) {
         Maximum69Number m69n = new Maximum69Number();
-        System.out.println(m69n.maximum69Number(9669));
+        System.out.println(m69n.maximum69Number(9696));
     }
 }
