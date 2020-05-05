1f7ec5f8f1c73c2fb61c1ff9774f77e86271bcd7
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-05 21:21:47 +0530

    frequency in array

--- src/FirstUniqueCharacter.java
+++ src/FirstUniqueCharacter.java
@@ -1,24 +1,21 @@
+
 public class FirstUniqueCharacter {
 
     public int firstUniqChar(String s) {
-        char[] alphabets = new char[26];
-        for (int i = 0; i < 26; i++) {
-            alphabets[i] = (char) ('a' + i);
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i) - 'a']++;
         }
-        int lastIndex = s.length();
-        int index = -1;
-        for (int i = 0; i < 26; i++) {
-            index = s.indexOf(alphabets[i]);
-            if (index >= 0 && index == s.lastIndexOf(alphabets[i])) {
-                index = Math.min(index, lastIndex);
-                lastIndex = index;
+        for (int i = 0; i < s.length(); i++) {
+            if (alphabet[s.charAt(i) - 'a'] == 1) {
+                return i;
             }
         }
-        return lastIndex == s.length() ? -1 : lastIndex;
+        return -1;
     }
 
     public static void main(String ags[]) {
         FirstUniqueCharacter fuc = new FirstUniqueCharacter();
-        System.out.println(fuc.firstUniqChar("aaaa"));
+        System.out.println(fuc.firstUniqChar("leetcode"));
     }
 }
