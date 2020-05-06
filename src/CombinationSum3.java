3e31cebd9c95573dc0cc67c5843262d2b189c976
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-06 20:53:03 +0530

    backtracking - without 0 - 9 numbers constraint

---
+++ src/CombinationSum3.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum3 {
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList();
+        backtracking(result, new ArrayList(), k, n, 1);
+        return result;
+    }
+
+    public void backtracking(List<List<Integer>> result, ArrayList<Integer> path, int k, int target, int index) {
+        if (k == 0 && target == 0) {
+            result.add(new ArrayList(path));
+        } else if (target < 0 || k < 0) {
+            return;
+        } else {
+            for (int i = index; i <= target; i++) {
+                path.add(i);
+                backtracking(result, path, k - 1, target - i, i + 1);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        CombinationSum3 cs3 = new CombinationSum3();
+        System.out.println(cs3.combinationSum3(3, 9));
+    }
+}
