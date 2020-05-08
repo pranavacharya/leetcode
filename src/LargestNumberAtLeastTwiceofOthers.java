44e0761a193e176e3d4ee8277de042217840482b
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-09 00:04:23 +0530

    ip array used instead of hashset

--- src/LargestNumberAtLeastTwiceofOthers.java
+++ src/LargestNumberAtLeastTwiceofOthers.java
@@ -1,21 +1,17 @@
 
-import java.util.HashSet;
-
 public class LargestNumberAtLeastTwiceofOthers {
 
     public int dominantIndex(int[] nums) {
-        HashSet<Integer> set = new HashSet();
         int max = Integer.MIN_VALUE;
         int index = -1;
         for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
             if (nums[i] > max) {
                 max = nums[i];
                 index = i;
             }
         }
-        for (int key : set) {
-            if (key != max && 2 * key > max) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i != index && 2 * nums[i] > max) {
                 return -1;
             }
         }
