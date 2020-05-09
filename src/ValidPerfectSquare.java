68a68293bb3eef8dfb855dc6a25ac7bcb296c49e
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-09 19:47:27 +0530

    binary search solution

--- src/ValidPerfectSquare.java
+++ src/ValidPerfectSquare.java
@@ -2,9 +2,16 @@
 public class ValidPerfectSquare {
 
     public boolean isPerfectSquare(int num) {
-        for (long i = 1; i * i <= num; i++) {
-            if (i * i == num) {
+        long low = 1;
+        long high = num;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (mid * mid == num) {
                 return true;
+            } else if (mid * mid < num) {
+                low = mid + 1;
+            } else if (mid * mid > num) {
+                high = mid - 1;
             }
         }
         return false;
