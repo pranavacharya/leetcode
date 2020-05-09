7999ca54cd290c9190f46a0901de3a8c7231f1ac
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-09 19:31:22 +0530

    Integer overflow- long used

--- src/ValidPerfectSquare.java
+++ src/ValidPerfectSquare.java
@@ -2,7 +2,7 @@
 public class ValidPerfectSquare {
 
     public boolean isPerfectSquare(int num) {
-        for (int i = 1; i * i <= num; i++) {
+        for (long i = 1; i * i <= num; i++) {
             if (i * i == num) {
                 return true;
             }
@@ -12,6 +12,7 @@ public class ValidPerfectSquare {
 
     public static void main(String args[]) {
         ValidPerfectSquare vps = new ValidPerfectSquare();
-        System.out.println(vps.isPerfectSquare(12));
+        System.out.println(vps.isPerfectSquare(16));
+        System.out.println(vps.isPerfectSquare(2147483647));
     }
 }
