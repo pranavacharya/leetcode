b2815a3287e7d95bc67993c80c00fafc7c756306
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-13 03:49:55 +0530

    O(log N)- binary search

--- src/SingleElementinaSortedArray.java
+++ src/SingleElementinaSortedArray.java
@@ -2,24 +2,20 @@
 public class SingleElementinaSortedArray {
 
     public int singleNonDuplicate(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        if (nums[0] != nums[1]) {
-            return nums[0];
-        }
-        for (int i = 1; i < nums.length - 2; i++) {
-            if (nums[i - 1] < nums[i] && nums[i] < nums[i + 1]) {
-                return nums[i];
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 != 0) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                high = mid - 1;
+            } else {
+                low = mid + 2;
             }
         }
-        if (nums[nums.length - 1] != nums[nums.length - 2]) {
-            return nums[nums.length - 1];
-        }
-        return -1;
+        return nums[low];
     }
 
     public static void main(String args[]) {
