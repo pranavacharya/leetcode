d2678fff9811239f604173bd3cacfab9b49876cd
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-16 19:59:56 +0530

    Kadane's algorithm

--- src/MaximumSumCircularSubarray.java
+++ src/MaximumSumCircularSubarray.java
@@ -2,30 +2,15 @@
 public class MaximumSumCircularSubarray {
 
     public int maxSubarraySumCircular(int[] A) {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i % A.length] < 0) {
-                max = Math.max(max, A[i % A.length]);
-                continue;
-            }
-            int sum = A[i];
-            max = Math.max(max, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
-            for (int j = i + 1; j < 2 * A.length; j++) {
-                if (j % A.length == i) {
-                    break;
-                }
-                sum += A[j % A.length];
-                max = Math.max(max, sum);
-                if (sum < 0) {
-                    sum = 0;
-                }
-
-            }
+        int total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
         }
-        return max;
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
     }
 
     public static void main(String args[]) {
