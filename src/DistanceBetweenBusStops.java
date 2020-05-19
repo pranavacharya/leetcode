bb576e4c69f4cec7cac3b242c78e67666f556388
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-19 23:50:15 +0530

    cyclic array traversal

---
+++ src/DistanceBetweenBusStops.java
@@ -0,0 +1,25 @@
+
+public class DistanceBetweenBusStops {
+
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int smaller = Math.min(start, destination);
+        int larger = Math.max(start, destination);
+        int a = 0;
+        for (int i = smaller; i < larger; i++) {
+            a += distance[i];
+        }
+        int b = 0;
+        int i = larger;
+        while (i % distance.length != smaller) {
+            b += distance[i % distance.length];
+            i++;
+        }
+        return Math.min(a, b);
+    }
+
+    public static void main(String args[]) {
+        DistanceBetweenBusStops dbbs = new DistanceBetweenBusStops();
+        int[] distance = new int[]{7, 10, 1, 12, 11, 14, 5, 0};
+        System.out.println(dbbs.distanceBetweenBusStops(distance, 7, 2));
+    }
+}
