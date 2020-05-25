0ad9b4672fbddf9f8970e0e2f1a3cace90490a85
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-25 23:46:19 +0530

    dynamic programming

---
+++ src/UncrossedLines.java
@@ -0,0 +1,24 @@
+
+public class UncrossedLines {
+
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int m = A.length, n = B.length, dp[][] = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String args[]) {
+        UncrossedLines ul = new UncrossedLines();
+        int[] A = new int[]{2, 5, 1, 2, 5};
+        int[] B = new int[]{10, 5, 2, 1, 5, 2};
+        System.out.println(ul.maxUncrossedLines(A, B));
+    }
+}
