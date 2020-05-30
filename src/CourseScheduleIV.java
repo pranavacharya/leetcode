50a26df7bfd7bee7f4acee9e4fede1c0ce1032ed
Pranav Acharya <apranav.acharya@gmail.com>
2020-05-30 22:11:29 +0530

    topological sort

--- src/CourseScheduleIV.java
+++ src/CourseScheduleIV.java
@@ -1,42 +1,55 @@
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class CourseScheduleIV {
 
     public List<Boolean> checkIfPrerequisite(int n, int[][] prerequisites, int[][] queries) {
-        int[][] links = new int[n][n];
         HashMap<Integer, ArrayList<Integer>> adj = new HashMap();
+        HashSet<Integer>[] allpre = new HashSet[n];
+        int[] indegree = new int[n];
         for (int[] pre : prerequisites) {
             ArrayList<Integer> temp = adj.getOrDefault(pre[0], new ArrayList());
             temp.add(pre[1]);
             adj.put(pre[0], temp);
+            indegree[pre[1]]++;
         }
-        for (int i = 0; i < n; i++) {
-            dfs(links, adj, i, new ArrayList());
-        }
-        List<Boolean> result = new ArrayList();
-        for (int[] q : queries) {
-            result.add(links[q[1]][q[0]] == 1);
-        }
-        return result;
-    }
 
-    private void dfs(int[][] links, HashMap<Integer, ArrayList<Integer>> adj, int node, ArrayList<Integer> path) {
-        for (int i : path) {
-            links[node][i] = 1;
+        Queue<Integer> queue = new LinkedList();
+        ArrayList<Integer> sorted = new ArrayList();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+            allpre[i] = new HashSet();
         }
-        if (!adj.containsKey(node)) {
-            return;
-        } else {
-            ArrayList<Integer> neighbours = adj.get(node);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sorted.add(current);
+            if (!adj.containsKey(current)) {
+                continue;
+            }
+            ArrayList<Integer> neighbours = adj.get(current);
             for (int nei : neighbours) {
-                path.add(node);
-                dfs(links, adj, nei, path);
-                path.remove(path.size() - 1);
+                allpre[nei].add(current);
+                allpre[nei].addAll(allpre[current]);
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.add(nei);
+                }
             }
         }
+
+        List<Boolean> result = new ArrayList();
+        for (int[] q : queries) {
+            result.add(allpre[q[1]].contains(q[0]));
+        }
+        return result;
     }
 
     public static void main(String args[]) {
