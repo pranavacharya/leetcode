4b2e242664344569a21970d621a1fb9bb3b6d913
Pranav Acharya <apranav.acharya@gmail.com>
2020-06-03 22:14:58 +0530

    layered bfs

--- src/MinimumHeightTrees.java
+++ src/MinimumHeightTrees.java
@@ -1,45 +1,54 @@
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.Queue;
 
 public class MinimumHeightTrees {
 
-    private int max = 0;
-
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         ArrayList<Integer>[] adj = new ArrayList[n];
+        int[] indegree = new int[n];
         for (int i = 0; i < adj.length; i++) {
             adj[i] = new ArrayList();
         }
         for (int[] e : edges) {
             adj[e[0]].add(e[1]);
             adj[e[1]].add(e[0]);
+            indegree[e[0]]++;
+            indegree[e[1]]++;
         }
-        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap();
-        for (int i = 0; i < n; i++) {
-            this.max = 0;
-            dfs(adj, new HashSet(), i, 0);
-            int local = this.max;
-            ArrayList<Integer> temp = map.getOrDefault(local, new ArrayList());
-            temp.add(i);
-            map.put(local, temp);
-        }
-
-        int shortest = map.firstKey();
-        return map.get(shortest);
-    }
-
-    public void dfs(ArrayList<Integer>[] adj, HashSet<Integer> visited, int node, int depth) {
-        visited.add(node);
-        for (int nei : adj[node]) {
-            if (visited.contains(nei)) {
-                continue;
+        ArrayList<Integer> result = new ArrayList();
+        Queue<Integer> queue = new LinkedList();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                result.add(i);
+            } else if (indegree[i] == 1) {
+                queue.offer(i);
             }
-            dfs(adj, visited, nei, depth + 1);
         }
-        max = Math.max(max, depth);
+        if (result.size() > 0) {
+            return result;
+        }
+        while (!queue.isEmpty()) {
+            result = new ArrayList();
+            int noOfLeaves = queue.size();
+            for (int i = 0; i < noOfLeaves; i++) {
+                int currentLeaf = queue.poll();
+                result.add(currentLeaf);
+                indegree[currentLeaf]--;
+                for (int nei : adj[currentLeaf]) {
+                    if (indegree[nei] == 0) {
+                        continue;
+                    }
+                    indegree[nei]--;
+                    if (indegree[nei] == 1) {
+                        queue.offer(nei);
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String args[]) {
