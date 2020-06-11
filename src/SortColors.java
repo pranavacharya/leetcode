3084df03afbafe95143b1a7f8cb69158c38f5183
Pranav Acharya <apranav.acharya@gmail.com>
2020-06-12 01:30:20 +0530

    three pointer solution

--- src/SortColors.java
+++ src/SortColors.java
@@ -4,25 +4,24 @@ import java.util.Arrays;
 public class SortColors {
 
     public void sortColors(int[] nums) {
-        int[] frequency = new int[3];
-        for (int i : nums) {
-            frequency[i]++;
-        }
-        int index = 0;
-        while (frequency[0] > 0) {
-            nums[index] = 0;
-            frequency[0]--;
-            index++;
-        }
-        while (frequency[1] > 0) {
-            nums[index] = 1;
-            frequency[1]--;
-            index++;
-        }
-        while (frequency[2] > 0) {
-            nums[index] = 2;
-            frequency[2]--;
-            index++;
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
         }
     }
 
