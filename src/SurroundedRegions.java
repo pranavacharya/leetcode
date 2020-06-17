1c8c33106aa0f90f8448a3d679c7c7fb8d59eafe
Pranav Acharya <apranav.acharya@gmail.com>
2020-06-17 23:14:25 +0530

    dfs

--- src/SurroundedRegions.java
+++ src/SurroundedRegions.java
@@ -8,31 +8,28 @@ public class SurroundedRegions {
     public void solve(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'O') {
-                    if (dfs(board, i, j)) {
-                        board[i][j] = 'X';
-                    };
+                if ((i == 0 || j == 0 || i == board.length - 1 || j == board[i].length - 1) && board[i][j] == 'O') {
+                    dfs(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 'B') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
                 }
             }
         }
     }
 
-    public boolean dfs(char[][] board, int row, int col) {
-        if (row == board.length - 1 || col == board[row].length - 1 || row == 0 || col == 0) {
-            return false;
-        } else {
-            boolean status = true;
-            board[row][col] = 'X';
-            for (int[] dir : dirs) {
-                if (board[row + dir[0]][col + dir[1]] == 'O') {
-                    status = status && dfs(board, row + dir[0], col + dir[1]);
-                }
-                if (!status) {
-                    break;
-                }
+    public void dfs(char[][] board, int row, int col) {
+        board[row][col] = 'B';
+        for (int[] dir : dirs) {
+            if (row + dir[0] < board.length && col + dir[1] < board[row].length && row + dir[0] >= 0 && col + dir[1] >= 0 && board[row + dir[0]][col + dir[1]] == 'O') {
+                dfs(board, row + dir[0], col + dir[1]);
             }
-            board[row][col] = 'O';
-            return status;
         }
     }
 
