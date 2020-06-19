e49063564b9fbe892967097d388461c83b9ef95b
Pranav Acharya <apranav.acharya@gmail.com>
2020-06-19 23:52:44 +0530

    divisor complement, find till square root

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -6,9 +6,14 @@ public class PerfectNumber {
             return false;
         }
         int sum = 0;
-        for (int i = 1; i <= num / 2; i++) {
+        for (int i = 1; i * i <= num; i++) {
             if (num % i == 0) {
-                sum += i;
+                if (num != i) {
+                    sum += i;
+                }
+                if (i * i != num && i != 1) {
+                    sum += (num / i);
+                }
             }
         }
         return num == sum;
@@ -16,6 +21,6 @@ public class PerfectNumber {
 
     public static void main(String args[]) {
         PerfectNumber pn = new PerfectNumber();
-        System.out.println(pn.checkPerfectNumber(29));
+        System.out.println(pn.checkPerfectNumber(1));
     }
 }
