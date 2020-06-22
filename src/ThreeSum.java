9adf8cb1b525d6372b4491d9403306614daabf9b
Pranav Acharya <apranav.acharya@gmail.com>
2020-06-23 01:50:39 +0530

    backtracking,TLE

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,54 +1,41 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Collections;
 
 public class ThreeSum {
 
-    private List<List<Integer>> output;
-
-    public ThreeSum() {
-        this.output = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList();
+        backtrack(result, new ArrayList(), nums, 0, 0);
+        return result;
     }
 
-    public void subset(ArrayList<Integer> soFar, ArrayList<Integer> rem) {
-        if (soFar.size() == 3) {
-            int sum = 0;
-            for (Integer i : soFar) {
-                sum += (int) i;
+    public void backtrack(List<List<Integer>> result, ArrayList<Integer> path, int[] nums, int index, int target) {
+        if (path.size() == 3) {
+            if (target == 0 && !result.contains(path)) {
+                result.add(new ArrayList(path));
             }
-            if (sum == 0) {
-                Collections.sort(soFar);
-                if (!output.contains(soFar)) {
-                    output.add(soFar);
+        } else {
+            for (int i = index; i < nums.length; i++) {
+                if (i != index && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] <= target) {
+                    path.add(nums[i]);
+                    backtrack(result, path, nums, i + 1, target - nums[i]);
+                    path.remove(path.size() - 1);
+                } else {
+                    break;
                 }
             }
-        } else if (!rem.isEmpty()) {
-            ArrayList<Integer> leftwith;
-            ArrayList<Integer> leftwithout;
-            ArrayList<Integer> right;
-            leftwith = (ArrayList<Integer>) soFar.clone();
-            leftwith.add(rem.get(0));
-            leftwithout = (ArrayList<Integer>) soFar.clone();
-            right = (ArrayList<Integer>) rem.clone();
-            right.remove(0);
-            subset(leftwith, right);
-            subset(leftwithout, right);
         }
-    }
-
-    public List<List<Integer>> threeSum(int[] nums) {
-        ThreeSum ts = new ThreeSum();
-        ArrayList inputlist = new ArrayList<>();
-        for (int in : nums) {
-            inputlist.add(in);
-        }
-        ts.subset(new ArrayList<>(), inputlist);
-        return ts.output;
     }
 
     public static void main(String args[]) {
         int[] input = {-1, 0, 1, 2, -1, -4};
+//        int[] input = {-4, -8, 7, 13, 10, 1, -14, -13, 0, 8, 6, -13, -5, -4, -12, 2, -11, 7, -5, 0, -9, -14, -8, -9, 2, -7, -13, -3, 13, 9, -14, -6, 8, 1, 14, -5, -13, 8, -10, -5, 1, 11, -11, 3, 14, -8, -10, -12, 6, -8, -5, 13, -15, 2, 11, -5, 10, 6, -1, 1, 0, 0, 2, -7, 8, -6, 3, 3, -13, 8, 5, -5, -3, 9, 5, -4, -14, 11, -8, 7, 10, -6, -3, 11, 12, -14, -9, -1, 7, 5, -15, 14, 12, -5, -8, -2, 4, 2, -14, -2, -12, 6, 8, 0, 0, -2, 3, -7, -14, 2, 7, 12, 12, 12, 0, 9, 13, -2, -15, -3, 10, -14, -4, 7, -12, 3, -10};
         ThreeSum ts = new ThreeSum();
         System.out.println(ts.threeSum(input));
     }
