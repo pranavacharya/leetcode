50131664429ccba50d04b5293623bc30873f059c
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-05 04:24:54 +0530

    dynamic programming

--- src/UglyNumber2.java
+++ src/UglyNumber2.java
@@ -1,32 +1,34 @@
 
+import java.util.ArrayList;
+
 public class UglyNumber2 {
 
     public int nthUglyNumber(int n) {
-        int count = 1;
-        int i = 1;
-        while (count <= n && count <= 1690) {
-            if (isUgly(i)) {
-                count++;
-            }
-            i++;
-        }
-        return i - 1;
-    }
-
-    public boolean isUgly(int n) {
         if (n == 0) {
-            return false;
+            return 0;
         }
-        while (n != 0 && n % 2 == 0) {
-            n /= 2;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        ArrayList<Integer> ugly = new ArrayList();
+        ugly.add(1);
+        while (ugly.size() < n) {
+            int fac2 = 2 * ugly.get(index2);
+            int fac3 = 3 * ugly.get(index3);
+            int fac5 = 5 * ugly.get(index5);
+            int min = Math.min(fac2, Math.min(fac3, fac5));
+            ugly.add(min);
+            if (min == fac2) {
+                index2++;
+            }
+            if (min == fac3) {
+                index3++;
+            }
+            if (min == fac5) {
+                index5++;
+            }
         }
-        while (n != 0 && n % 3 == 0) {
-            n /= 3;
-        }
-        while (n != 0 && n % 5 == 0) {
-            n /= 5;
-        }
-        return 1 == n;
+        return ugly.get(ugly.size() - 1);
     }
 
     public static void main(String args[]) {
