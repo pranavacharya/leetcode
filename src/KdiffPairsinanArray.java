b8e97a9a45a57abdb014dcc7daa51dc54928e3b7
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-09 01:59:27 +0530

    two pointers

--- src/KdiffPairsinanArray.java
+++ src/KdiffPairsinanArray.java
@@ -1,27 +1,35 @@
 
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class KdiffPairsinanArray {
 
     public int findPairs(int[] nums, int k) {
         int count = 0;
         Arrays.sort(nums);
-        HashSet<Integer> set = new HashSet();
-        HashSet<String> visited = new HashSet();
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i] - k) && !visited.contains("" + nums[i] + "," + (nums[i] - k))) {
+        int i = 0;
+        int j = 0;
+        while (j < nums.length) {
+            if (i == j) {
+                j++;
+            } else if (nums[j] - nums[i] == k) {
                 count++;
-                visited.add("" + nums[i] + "," + (nums[i] - k));
+                while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                    j++;
+                }
+                j++;
+            } else if (nums[j] - nums[i] < k) {
+                j++;
+            } else {
+                i++;
             }
-            set.add(nums[i]);
         }
         return count;
     }
 
     public static void main(String args[]) {
         KdiffPairsinanArray kdpia = new KdiffPairsinanArray();
-        int[] nums = new int[]{3, 1, 4, 1, 5};
-        System.out.println(kdpia.findPairs(nums, 2));
+        int[] nums = new int[]{1, 1, 1, 2, 2};
+//        int[] nums = new int[]{3, 1, 4, 1, 5};
+        System.out.println(kdpia.findPairs(nums, 1));
     }
 }
