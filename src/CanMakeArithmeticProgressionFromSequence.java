9d252512c57c08e4dca117b0071d85c67bf18293
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-15 22:46:49 +0530

    sort and compare

---
+++ src/CanMakeArithmeticProgressionFromSequence.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class CanMakeArithmeticProgressionFromSequence {
+
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] != arr[i] - arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        CanMakeArithmeticProgressionFromSequence cmapfs = new CanMakeArithmeticProgressionFromSequence();
+        int[] arr = new int[]{1, 2, 4};
+        System.out.println(cmapfs.canMakeArithmeticProgression(arr));
+    }
+}
