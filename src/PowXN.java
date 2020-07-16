37c54ccbe272980a01fe1fd50bc0e6857baa3a34
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-16 22:41:45 +0530

    recursive solution

--- src/PowXN.java
+++ src/PowXN.java
@@ -2,15 +2,25 @@
 public class PowXN {
 
     public double myPow(double x, int n) {
-        double ans = 1;
-        for (int i = 0; i < Math.abs(n); i++) {
-            ans *= x;
+        return myPow(x, 1L * n);
+    }
+
+    public double myPow(double x, long n) {
+        if (n == 0) {
+            return 1;
+        } else if (n < 0) {
+            return myPow(1 / x, -n);
+        } else {
+            if (n % 2 == 0) {
+                return myPow(x * x, n / 2);
+            } else {
+                return x * myPow(x * x, n / 2);
+            }
         }
-        return n < 0 ? 1 / ans : ans;
     }
 
     public static void main(String args[]) {
         PowXN p = new PowXN();
-        System.out.println(p.myPow(2, -2));
+        System.out.println(p.myPow(2, 10));
     }
 }
