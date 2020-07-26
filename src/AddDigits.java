a4db2a994494e049834b63db88151f0b5921c3a0
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-27 04:26:17 +0530

    digital root

--- src/AddDigits.java
+++ src/AddDigits.java
@@ -2,15 +2,13 @@
 public class AddDigits {
 
     public int addDigits(int num) {
-        while (num > 9) {
-            int sum = 0;
-            while (num > 0) {
-                sum += (num % 10);
-                num = num / 10;
-            }
-            num = sum;
+        if (num == 0) {
+            return 0;
         }
-        return num;
+        if (num % 9 == 0) {
+            return 9;
+        }
+        return num % 9;
     }
 
     public static void main(String args[]) {
