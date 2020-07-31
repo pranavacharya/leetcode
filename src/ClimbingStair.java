4fa6831a87ac56b88b8130c2462161faed04d480
Pranav Acharya <apranav.acharya@gmail.com>
2020-07-31 21:38:16 +0530

    dynamic programming

--- src/ClimbingStair.java
+++ src/ClimbingStair.java
@@ -2,13 +2,28 @@
 public class ClimbingStair {
 
     public int climbStairs(int n) {
-        double sqrt5=Math.sqrt(5);
-        double fibn=Math.pow((1+sqrt5)/2,n+1)-Math.pow((1-sqrt5)/2,n+1);
-        return (int)(fibn/sqrt5);
+        if (n < 3) {
+            if (n == 1) {
+                return 1;
+            } else if (n == 2) {
+                return 2;
+            } else {
+                return 0;
+            }
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 
     public static void main(String args[]) {
         ClimbingStair cs = new ClimbingStair();
         System.out.println(cs.climbStairs(44));
+        System.out.println(cs.climbStairs(3));
     }
 }
