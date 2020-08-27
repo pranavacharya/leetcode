62fbffa41556caf5f9be8a3296d79539b216f0fe
Pranav Acharya <apranav.acharya@gmail.com>
2020-08-27 23:14:01 +0530

    TreeMap solution

--- src/FindRightInterval.java
+++ src/FindRightInterval.java
@@ -1,37 +1,21 @@
 
 import java.util.Arrays;
+import java.util.TreeMap;
 
 public class FindRightInterval {
 
     public int[] findRightInterval(int[][] intervals) {
-        int[][] interval = new int[intervals.length][3];
+        TreeMap<Integer, Integer> map = new TreeMap();
         for (int i = 0; i < intervals.length; i++) {
-            for (int j = 0; j < intervals[i].length; j++) {
-                interval[i][j] = intervals[i][j];
-            }
-            interval[i][2] = i;
+            map.put(intervals[i][0], i);
         }
-        Arrays.sort(interval, (a, b) -> Double.compare(a[0], b[0]));
         int[] res = new int[intervals.length];
-        for (int i = 0; i < interval.length; i++) {
-            int find = intervals[i][1];
-            int low = 0;
-            int high = interval.length - 1;
-            while (low < high) {
-                int mid = low + (high - low) / 2;
-                if (interval[mid][0] == find) {
-                    low = mid;
-                    break;
-                } else if (interval[mid][0] < find) {
-                    low = low + 1;
-                } else {
-                    high = mid;
-                }
-            }
-            if (interval[low][0] < find) {
-                res[i] = -1;
+        for (int i = 0; i < intervals.length; i++) {
+
+            if (map.ceilingKey(intervals[i][1]) != null) {
+                res[i] = map.get(map.ceilingKey(intervals[i][1]));
             } else {
-                res[i] = interval[low][2];
+                res[i] = -1;
             }
         }
         return res;
