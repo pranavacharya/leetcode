c212b33458791a7e0fb1156b1bcac8af972b8648
Pranav Acharya <apranav.acharya@gmail.com>
2020-09-16 22:27:51 +0530

    trie solution

--- src/MaximumXORofTwoNumbersinanArray.java
+++ src/MaximumXORofTwoNumbersinanArray.java
@@ -1,14 +1,50 @@
 
 public class MaximumXORofTwoNumbersinanArray {
 
+    private class TrieNode {
+
+        private TrieNode[] nodes;
+
+        private TrieNode() {
+            this.nodes = new TrieNode[2];
+        }
+    }
+
     public int findMaximumXOR(int[] nums) {
-        int max = 0;
+        TrieNode root = new TrieNode();
+
+        TrieNode curr;
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                max = Math.max(max, nums[i] ^ nums[j]);
+            curr = root;
+            for (int j = 30; j >= 0; j--) {
+                int bit = getIthBit(nums[i], j);
+                if (curr.nodes[bit ^ 1] == null) {
+                    curr.nodes[bit ^ 1] = new TrieNode();
+                }
+                curr = curr.nodes[bit ^ 1];
             }
         }
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curr = root;
+            int local = 0;
+            for (int j = 30; j >= 0; j--) {
+                int bit = getIthBit(nums[i], j);
+                if (curr.nodes[bit] != null) {
+                    curr = curr.nodes[bit];
+                    local += (1 << j);
+                } else {
+                    curr = curr.nodes[bit ^ 1];
+                }
+            }
+            max = Math.max(max, local);
+        }
         return max;
+    }
+
+    private int getIthBit(int num, int i) {
+        return ((num & (1 << i)) == 0) ? 0 : 1;
     }
 
     public static void main(String args[]) {
