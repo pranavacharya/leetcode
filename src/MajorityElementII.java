1bee3e9fcdcb616eff62e4c1b0ba83e6bb3dd6b3
Pranav Acharya <apranav.acharya@gmail.com>
2020-09-22 23:54:32 +0530

    Boyer-Moore Voting Algorithm

--- src/MajorityElementII.java
+++ src/MajorityElementII.java
@@ -1,21 +1,56 @@
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class MajorityElementII {
 
-    public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList();
-        HashMap<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        for (int key : map.keySet()) {
-            if (map.get(key) > (nums.length / 3)) {
-                result.add(key);
+    public List< Integer> majorityElement(int[] nums) {
+
+        int count1 = 0;
+        int count2 = 0;
+
+        Integer candidate1 = null;
+        Integer candidate2 = null;
+
+        for (int n : nums) {
+            if (candidate1 != null && candidate1 == n) {
+                count1++;
+            } else if (candidate2 != null && candidate2 == n) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = n;
+                count1++;
+            } else if (count2 == 0) {
+                candidate2 = n;
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
         }
+
+        List result = new ArrayList<>();
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int n : nums) {
+            if (candidate1 != null && n == candidate1) {
+                count1++;
+            }
+            if (candidate2 != null && n == candidate2) {
+                count2++;
+            }
+        }
+
+        int n = nums.length;
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
         return result;
     }
 
