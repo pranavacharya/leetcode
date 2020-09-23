a2c6ac630bd8eee9c043bc9edf9cb21b55e663d5
Pranav Acharya <apranav.acharya@gmail.com>
2020-09-23 23:51:41 +0530

    linear solution

--- src/GasStation.java
+++ src/GasStation.java
@@ -2,36 +2,35 @@
 public class GasStation {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int i;
-        for (i = 0; i < gas.length; i++) {
-            if (gas[i] < cost[i]) {
-                continue;
-            }
-            int j = i;
-            int rc = gas[i] - cost[i];
-            do {
-                j++;
-                j = (j % gas.length);
-                if (gas[j] + rc < cost[j]) {
-                    break;
-                }
-                rc = rc + gas[j] - cost[j];
-                if (j == i - 1 || (i == 0 && j == gas.length - 1)) {
-                    return i;
-                }
-            } while (j != i);
+        int tank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            tank += gas[i] - cost[i];
         }
-        return -1;
+        if (tank < 0) {
+            return -1;
+        }
+        int start = 0;
+        int acc = 0;
+        for (int i = 0; i < gas.length; i++) {
+            int gain = gas[i] - cost[i];
+            if (acc + gain < 0) {
+                start = i + 1;
+                acc = 0;
+            } else {
+                acc = acc + gain;
+            }
+        }
+        return start;
     }
 
     public static void main(String args[]) {
         GasStation gs = new GasStation();
-//        int[] gas = new int[]{3, 1, 1};
-//        int[] cost = new int[]{1, 2, 2};
+        int[] gas = new int[]{3, 1, 1};
+        int[] cost = new int[]{1, 2, 2};
 //        int[] gas = new int[]{2, 3, 4};
 //        int[] cost = new int[]{3, 4, 3};
-        int[] gas = new int[]{1, 2, 3, 4, 5};
-        int[] cost = new int[]{3, 4, 5, 1, 2};
+//        int[] gas = new int[]{1, 2, 3, 4, 5};
+//        int[] cost = new int[]{3, 4, 5, 1, 2};
         System.out.println(gs.canCompleteCircuit(gas, cost));
     }
 }
