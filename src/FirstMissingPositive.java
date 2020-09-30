4c549f81ff99c75a920778c9a718c1f3e59f8047
Pranav Acharya <apranav.acharya@gmail.com>
2020-09-30 23:48:01 +0530

    O(N) time and space

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -1,25 +1,36 @@
 
-import java.util.HashSet;
-
 public class FirstMissingPositive {
 
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set = new HashSet();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                set.add(nums[i]);
+            if (nums[i] > nums.length || nums[i] <= 0) {
+                nums[i] = nums.length + 1;
             }
         }
-        int ans = 1;
-        while (set.contains(ans)) {
-            ans++;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < nums.length + 1) {
+                int index = Math.abs(nums[i]) - 1;
+                if (index >= nums.length) {
+                    continue;
+                }
+                if (nums[index] > 0) {
+                    nums[index] = -1 * nums[index];
+                }
+            }
         }
-        return ans;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
     }
 
     public static void main(String args[]) {
         FirstMissingPositive pmp = new FirstMissingPositive();
-        int[] nums = new int[]{3, 4, -1, 1};
+        int[] nums = new int[]{7, 8, 9, 11, 12};
         System.out.println(pmp.firstMissingPositive(nums));
     }
 }
