61dcf2cd2c852f88588500b2f9289747f7d2c3d4
Pranav Acharya <apranav.acharya@gmail.com>
2020-10-12 00:26:53 +0530

    two pointers

--- src/SplitTwoStringstoMakePalindrome.java
+++ src/SplitTwoStringstoMakePalindrome.java
@@ -2,20 +2,30 @@
 public class SplitTwoStringstoMakePalindrome {
 
     public boolean checkPalindromeFormation(String a, String b) {
-        for (int i = 0; i < a.length(); i++) {
-            String first = a.substring(0, i) + b.substring(i);
-            String second = b.substring(0, i) + a.substring(i);
-            if (checkIfPalindrom(first) || checkIfPalindrom(second)) {
-                return true;
-            }
-        }
-        return false;
+        return checkCombination(a, b) || checkCombination(b, a);
     }
 
-    public boolean checkIfPalindrom(String s) {
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+    private boolean checkCombination(String a, String b) {
+        int i = 0;
+        int j = a.length() - 1;
+        while (i <= j) {
+            if (a.charAt(i) == b.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                break;
+            }
+        }
+        return (checkIfPalindrom(a, i, j) || checkIfPalindrom(b, i, j));
+    }
+
+    public boolean checkIfPalindrom(String s, int i, int j) {
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
+            } else {
+                i++;
+                j--;
             }
         }
         return true;
@@ -23,6 +33,6 @@ public class SplitTwoStringstoMakePalindrome {
 
     public static void main(String args[]) {
         SplitTwoStringstoMakePalindrome stsmp = new SplitTwoStringstoMakePalindrome();
-        System.out.println(stsmp.checkPalindromeFormation("x", "y"));
+        System.out.println(stsmp.checkPalindromeFormation("ulacfd", "jizalu"));
     }
 }
