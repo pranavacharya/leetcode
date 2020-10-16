936d48a40938f02a050f1fd2c0c4e0dcc3321c64
Pranav Acharya <apranav.acharya@gmail.com>
2020-10-16 23:25:05 +0530

    binary search

--- src/FindPeakElement.java
+++ src/FindPeakElement.java
@@ -5,20 +5,17 @@ public class FindPeakElement {
         if (nums.length == 0 || nums.length == 1) {
             return 0;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] > nums[i + 1]) {
-                    return i;
-                }
-            } else if (i == nums.length - 1) {
-                if (nums[i] > nums[i - 1]) {
-                    return i;
-                }
-            } else if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                return i;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        return 0;
+        return low;
     }
 
     public static void main(String args[]) {
