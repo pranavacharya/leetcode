774f28c50cef9de3f80a4e3da77de57eb1c0b272
Pranav Acharya <apranav.acharya@gmail.com>
2020-11-16 23:34:56 +0530

    O(N) solution

--- src/LongestMountaininArray.java
+++ src/LongestMountaininArray.java
@@ -3,24 +3,25 @@ public class LongestMountaininArray {
 
     public int longestMountain(int[] A) {
         int max = 0;
-        for (int i = 1; i < A.length - 1; i++) {
-            max = Math.max(max, expandFromCenter(i, A));
+        int index = 0;
+        while (index < A.length - 1) {
+            int start = index;
+            while (index + 1 < A.length && A[index] < A[index + 1]) {
+                index++;
+            }
+            boolean down = false;
+            while (start != index && index + 1 < A.length && A[index + 1] < A[index]) {
+                index++;
+                down = true;
+            }
+            int end = index;
+            int dist = (end != start && down) ? end - start + 1 : 0;
+            max = Math.max(max, dist);
+            if (end == start) {
+                index++;
+            }
         }
         return max;
-    }
-
-    private int expandFromCenter(int index, int[] A) {
-        int i = index, j = index;
-        while (i - 1 >= 0 && A[i] > A[i - 1]) {
-            i--;
-        }
-        while (j + 1 < A.length && A[j] > A[j + 1]) {
-            j++;
-        }
-        if (i == index || j == index) {
-            return 0;
-        }
-        return i != j ? j - i + 1 : 0;
     }
 
     public static void main(String args[]) {
