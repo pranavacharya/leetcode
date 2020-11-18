b974ae7b61fe1a34dd7967edb566392d277328c3
Pranav Acharya <apranav.acharya@gmail.com>
2020-11-18 16:40:47 +0530

    sort and greedy solution

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -4,54 +4,36 @@ import java.util.Arrays;
 
 public class MergeIntervals {
 
-    class obj {
-
-        int a;
-        int b;
-
-        public obj(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-    }
-
     public int[][] merge(int[][] intervals) {
-        ArrayList<obj> result = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int i = 0;
-        int j = 0;
-
-        while (i < intervals.length) {
-            obj latest = new obj(intervals[i][0], intervals[i][1]);
-            while (j + 1 < intervals.length) {
-                if (latest.b > intervals[j + 1][1]) {
-                    j++;
-                    continue;
-                }
-                if (intervals[j + 1][0] <= latest.b) {
-                    latest.b = intervals[j + 1][1];
-                    j++;
-                } else {
-                    break;
-                }
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        if (intervals.length == 0) {
+            return new int[][]{};
+        }
+        ArrayList<int[]> list = new ArrayList();
+        int[] curr = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > curr[1]) {
+                list.add(curr);
+                curr = intervals[i];
             }
-            i = ++j;
-            result.add(latest);
+            if (intervals[i][1] < curr[1]) {
+                continue;
+            }
+            if (intervals[i][0] <= curr[1]) {
+                curr[1] = intervals[i][1];
+            }
         }
-        int[][] res = new int[result.size()][];
-        int index = 0;
-        for (obj o : result) {
-            res[index] = new int[]{o.a, o.b};
-            index++;
+        list.add(curr);
+        int[][] result = new int[list.size()][];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
         }
-        return res;
+        return result;
     }
 
     public static void main(String args[]) {
         MergeIntervals mi = new MergeIntervals();
-//        int[][] intervals = new int[][]{{2, 3}, {4, 5}, {6, 7}, {8, 9}, {1, 10}};
         int[][] intervals = new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-//        int[][] intervals = new int[][]{{1, 4}, {4, 5}};
         System.out.println(Arrays.deepToString(mi.merge(intervals)));
     }
 }
