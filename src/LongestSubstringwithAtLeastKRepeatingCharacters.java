101e59b9615b75fc78c9f437486f0e9000b5d10c
Pranav Acharya <apranav.acharya@gmail.com>
2020-11-26 23:57:33 +0530

    sliding window

--- src/LongestSubstringwithAtLeastKRepeatingCharacters.java
+++ src/LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -2,37 +2,58 @@
 public class LongestSubstringwithAtLeastKRepeatingCharacters {
 
     public int longestSubstring(String s, int k) {
-        String ans = "";
-        for (int i = 0; i <= s.length(); i++) {
-            for (int j = i; j <= s.length(); j++) {
-                if (isValidString(s.substring(i, j), k)) {
-                    if (ans.length() < s.subSequence(i, j).length()) {
-                        ans = s.substring(i, j);
+        int ans = 0;
+        int unique = findUniqueCharacters(s);
+        int[] freq;
+        for (int u = 1; u <= unique; u++) {
+            freq = new int[26];
+            int i = 0;
+            int j = 0;
+            int uc = 0;
+            int ks = 0;
+            while (j < s.length()) {
+                if (uc <= u) {
+                    if (freq[s.charAt(j) - 'a'] == 0) {
+                        uc++;
                     }
+                    freq[s.charAt(j) - 'a']++;
+                    if (freq[s.charAt(j) - 'a'] == k) {
+                        ks++;
+                    }
+                    j++;
+                } else {
+                    if (freq[s.charAt(i) - 'a'] == k) {
+                        ks--;
+                    }
+                    freq[s.charAt(i) - 'a']--;
+                    if (freq[s.charAt(i) - 'a'] == 0) {
+                        uc--;
+                    }
+                    i++;
+                }
+
+                if (uc == u && uc == ks) {
+                    ans = Math.max(ans, j - i);
                 }
             }
         }
-        return ans.length();
+        return ans;
     }
 
-    private boolean isValidString(String s, int k) {
+    private int findUniqueCharacters(String s) {
+        int unique = 0;
         int[] freq = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] == 0) {
-                continue;
-            }
-            if (freq[i] < k) {
-                return false;
+            if (freq[s.charAt(i) - 'a'] == 0) {
+                freq[s.charAt(i) - 'a']++;
+                unique++;
             }
         }
-        return true;
+        return unique;
     }
 
     public static void main(String args[]) {
         LongestSubstringwithAtLeastKRepeatingCharacters lslkrc = new LongestSubstringwithAtLeastKRepeatingCharacters();
-        System.out.println(lslkrc.longestSubstring("aaabbb", 3));
+        System.out.println(lslkrc.longestSubstring("aabb", 2));
     }
 }
