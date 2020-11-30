1a0c3ffada6594e59c36883d5ca23d8f71695473
Pranav Acharya <apranav.acharya@gmail.com>
2020-11-30 23:19:13 +0530

    stack solution

--- src/FindtheMostCompetitiveSubsequence.java
+++ src/FindtheMostCompetitiveSubsequence.java
@@ -1,38 +1,34 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class FindtheMostCompetitiveSubsequence {
 
     public int[] mostCompetitive(int[] nums, int k) {
-        ArrayList<Integer> list = new ArrayList();
-        helper(nums, k, 0, nums.length - k, list);
-        int[] res = list.stream().mapToInt(i -> i).toArray();
-        return res;
-    }
-
-    private void helper(int[] nums, int k, int start, int end, ArrayList<Integer> results) {
-        if (k == 0) {
-            return;
-        }
-
-        int minIndex = -1;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = start; i <= end; i++) {
-            if (min > nums[i]) {
-                min = nums[i];
-                minIndex = i;
+        Stack<Integer> stack = new Stack();
+        for (int i = 0; i < nums.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(nums[i]);
+            } else if (stack.peek() <= nums[i]) {
+                stack.push(nums[i]);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > nums[i] && (nums.length - i - 1 + stack.size() >= k)) {
+                    stack.pop();
+                }
+                stack.push(nums[i]);
             }
         }
-
-        results.add(min);
-        helper(nums, k - 1, minIndex + 1, nums.length - (k - 1), results);
+        while (!stack.isEmpty() && stack.size() != k) {
+            stack.pop();
+        }
+        ArrayList<Integer> list = new ArrayList(stack);
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
     public static void main(String args[]) {
         FindtheMostCompetitiveSubsequence fmcs = new FindtheMostCompetitiveSubsequence();
-        int[] nums = new int[]{2, 4, 3, 3, 5, 4, 9, 6};
-        System.out.println(Arrays.toString(fmcs.mostCompetitive(nums, 4)));
+        int[] nums = new int[]{71, 18, 52, 29, 55, 73, 24, 42, 66, 8, 80, 2};
+        System.out.println(Arrays.toString(fmcs.mostCompetitive(nums, 3)));
     }
 }
