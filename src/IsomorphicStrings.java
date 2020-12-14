465a8b018816f8c0f1787e7f8d4478c9b4c24a0d
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-14 23:32:10 +0530

    hashset hashmap solution

---
+++ src/IsomorphicStrings.java
@@ -0,0 +1,33 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class IsomorphicStrings {
+
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Character> map = new HashMap();
+        HashSet<Character> set = new HashSet();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (t.charAt(i) != map.get(s.charAt(i))) {
+                    return false;
+                }
+            } else {
+                if (set.contains(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+                set.add(t.charAt(i));
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        IsomorphicStrings is = new IsomorphicStrings();
+        System.out.println(is.isIsomorphic("egg", "add"));
+    }
+}
