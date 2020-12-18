80a9048ccc7a39724e1d98b199952e915dab9e01
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-18 23:43:53 +0530

    O(n^2) solution

--- src/IncreasingTripletSubsequence.java
+++ src/IncreasingTripletSubsequence.java
@@ -2,14 +2,14 @@
 public class IncreasingTripletSubsequence {
 
     public boolean increasingTriplet(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] > nums[i]) {
-                    for (int k = j + 1; k < nums.length; k++) {
-                        if (nums[k] > nums[j]) {
-                            return true;
-                        }
-                    }
+        boolean[] dp = new boolean[nums.length];
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && nums[i] > nums[j]) {
+                    return true;
+                }
+                if (nums[i] > nums[j]) {
+                    dp[i] = true;
                 }
             }
         }
