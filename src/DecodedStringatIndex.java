228a40231cc580cd07ae79f14f7728f4a77bb554
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-23 03:44:52 +0530

    math solution, O(1) space complexity

--- src/DecodedStringatIndex.java
+++ src/DecodedStringatIndex.java
@@ -2,25 +2,26 @@
 public class DecodedStringatIndex {
 
     public String decodeAtIndex(String S, int K) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < S.length(); i++) {
-            if (Character.isAlphabetic(S.charAt(i))) {
-                sb.append(S.charAt(i));
-                if (K == sb.length()) {
-                    return "" + sb.charAt(sb.length() - 1);
-                }
+        long size = 0;
+        int n = S.length();
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(S.charAt(i))) {
+                size *= Character.getNumericValue(S.charAt(i));
             } else {
-                int len = sb.length();
-                int times = Character.getNumericValue(S.charAt(i));
-                if (len * times < K) {
-                    String temp = sb.toString();
-                    for (int j = 0; j < times - 1; j++) {
-                        sb.append(temp);
-                    }
-                } else {
-                    int index = (K - 1) % len;
-                    return "" + sb.charAt(index);
-                }
+                size++;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            K %= size;
+            char c = S.charAt(i);
+            if (K == 0 && Character.isAlphabetic(c)) {
+                return Character.toString(c);
+            }
+            if (Character.isDigit(c)) {
+                size /= Character.getNumericValue(c);
+            } else {
+                size--;
             }
         }
         return "";
@@ -28,6 +29,6 @@ public class DecodedStringatIndex {
 
     public static void main(String args[]) {
         DecodedStringatIndex dsi = new DecodedStringatIndex();
-        System.out.println(dsi.decodeAtIndex("leet2code3", 10));
+        System.out.println(dsi.decodeAtIndex("y959q969u3hb22odq595", 222280369));
     }
 }
