0c530539de515933f2b3fb5de7480d728635c1fc
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-29 19:40:48 +0530

    dfs

--- src/PseudoPalindromicPathsinaBinaryTree.java
+++ src/PseudoPalindromicPathsinaBinaryTree.java
@@ -5,51 +5,40 @@ public class PseudoPalindromicPathsinaBinaryTree {
 
     private int count;
 
-    public PseudoPalindromicPathsinaBinaryTree() {
-        this.count = 0;
-    }
-
     public int pseudoPalindromicPaths(TreeNode root) {
+        this.count = 0;
         if (root == null) {
-            return 0;
+            return this.count;
         }
-        dfs(root, 1, new HashMap());
+        dfs(root, new HashMap());
         return this.count;
     }
 
-    public void dfs(TreeNode root, int depth, HashMap<Integer, Integer> frequency) {
-        if (root.left == null && root.right == null) {
-            frequency.put(root.val, frequency.getOrDefault(root.val, 0) + 1);
-            if (ispseudoPalindrome(frequency, depth)) {
-                this.count++;
-            }
-            frequency.put(root.val, frequency.getOrDefault(root.val, 0) - 1);
-            if (frequency.get(root.val) == 0) {
-                frequency.remove(root.val);
-            }
-        } else {
-            frequency.put(root.val, frequency.getOrDefault(root.val, 0) + 1);
-            if (root.left != null) {
-                dfs(root.left, depth + 1, frequency);
-            }
-            if (root.right != null) {
-                dfs(root.right, depth + 1, frequency);
-            }
-            frequency.put(root.val, frequency.get(root.val) - 1);
-            if (frequency.get(root.val) == 0) {
-                frequency.remove(root.val);
-            }
+    private void dfs(TreeNode root, HashMap<Integer, Integer> map) {
+        map.put(root.val, map.getOrDefault(root.val, 0) + 1);
+        if (root.left == null && root.right == null && isPseudoPalindrome(map)) {
+            this.count++;
+        }
+        if (root.left != null) {
+            dfs(root.left, map);
+        }
+        if (root.right != null) {
+            dfs(root.right, map);
+        }
+        map.put(root.val, map.get(root.val) - 1);
+        if (map.get(root.val) == 0) {
+            map.remove(root.val);
         }
     }
 
-    public boolean ispseudoPalindrome(HashMap<Integer, Integer> frequency, int depth) {
-        int oddNumbers = 0;
-        for (int key : frequency.keySet()) {
-            if (frequency.get(key) % 2 == 1) {
-                oddNumbers++;
+    private boolean isPseudoPalindrome(HashMap<Integer, Integer> map) {
+        int odd = 0;
+        for (int key : map.keySet()) {
+            if (map.get(key) % 2 != 0) {
+                odd++;
             }
         }
-        return !((depth % 2 == 0 && oddNumbers > 0) || (depth % 2 == 1 && oddNumbers > 1));
+        return odd == 0 || odd == 1;
     }
 
     public static void main(String args[]) {
@@ -60,6 +49,6 @@ public class PseudoPalindromicPathsinaBinaryTree {
         root.right.right = new TreeNode(1);
         root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(1);
-        System.out.println(pppibt.pseudoPalindromicPaths(null));
+        System.out.println(pppibt.pseudoPalindromicPaths(root));
     }
 }
