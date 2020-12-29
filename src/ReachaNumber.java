77bb9c7a413507dee8f98d33b24af2669c8442fd
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-30 02:45:31 +0530

    math solution

--- src/ReachaNumber.java
+++ src/ReachaNumber.java
@@ -1,27 +1,17 @@
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class ReachaNumber {
 
     public int reachNumber(int target) {
-        Queue<Integer> queue = new LinkedList();
-        queue.add(0);
-        int step = 1;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size > 0) {
-                int curr = queue.poll();
-                if (curr == target) {
-                    return step - 1;
-                }
-                queue.add(curr + step);
-
-                queue.add(curr - step);
-
-                size--;
-            }
+        target = Math.abs(target);
+        int step = 0;
+        int sum = 0;
+        while (sum < target) {
             step++;
+            sum += step;
+        }
+        while ((sum - target) % 2 != 0) {
+            step++;
+            sum += step;
         }
         return step;
     }
