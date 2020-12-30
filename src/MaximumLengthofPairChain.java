a01967a5e848502025e596f4d4daf75754bd7dfe
Pranav Acharya <apranav.acharya@gmail.com>
2020-12-31 01:42:17 +0530

    dynamic programming

--- src/MaximumLengthofPairChain.java
+++ src/MaximumLengthofPairChain.java
@@ -4,22 +4,20 @@ import java.util.Arrays;
 public class MaximumLengthofPairChain {
 
     public int findLongestChain(int[][] pairs) {
-        if (pairs.length == 0) {
-            return 0;
-        }
         Arrays.sort(pairs, (a, b) -> Double.compare(a[0], b[0]));
         int[] dp = new int[pairs.length];
+        Arrays.fill(dp, 1);
         dp[0] = 1;
+        int max = 0;
         for (int i = 1; i < pairs.length; i++) {
             for (int j = i - 1; j >= 0; j--) {
-                if (pairs[j][1] < pairs[i][0]) {
+                if (pairs[i][0] > pairs[j][1]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
-                    break;
                 }
-                dp[i] = Math.max(dp[i], dp[j]);
             }
+            max = Math.max(max, dp[i]);
         }
-        return dp[dp.length - 1];
+        return max;
     }
 
     public static void main(String args[]) {
