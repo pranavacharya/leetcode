6961e2716af79bc04c589b9f25b9639f98a3b7d0
Pranav Acharya <apranav.acharya@gmail.com>
2021-01-04 23:05:27 +0530

    linear solution

--- src/SortArrayByParityII.java
+++ src/SortArrayByParityII.java
@@ -1,30 +1,23 @@
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class SortArrayByParityII {
 
     public int[] sortArrayByParityII(int[] A) {
-        int[] ans = new int[A.length];
-        HashMap<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < A.length; i++) {
-            map.put(A[i], map.getOrDefault(A[i], 0) + 1);
-        }
-        int i = 0;
         int j = 1;
-        for (int key : map.keySet()) {
-            int count = map.get(key);
-            for (int k = 0; k < count; k++) {
-                if (key % 2 == 0) {
-                    ans[i] = key;
-                    i += 2;
-                } else {
-                    ans[j] = key;
+        for (int i = 0; i < A.length; i += 2) {
+            if (A[i] % 2 == 1) {
+                while (A[j] % 2 == 1) {
                     j += 2;
                 }
+
+                int tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
             }
         }
-        return ans;
+
+        return A;
     }
 
     public static void main(String args[]) {
