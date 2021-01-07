5e9f13e9afee1dc132c5c5d6f37335a8b65dd5aa
Pranav Acharya <apranav.acharya@gmail.com>
2021-01-07 20:46:31 +0530

    dynamic programming

--- src/PartitionEqualSubsetSum.java
+++ src/PartitionEqualSubsetSum.java
@@ -3,21 +3,32 @@ public class PartitionEqualSubsetSum {
 
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
         if (sum % 2 != 0) {
             return false;
         }
-        sum = sum / 2;
-        boolean[] dp = new boolean[sum + 1];
-        dp[0] = true;
-        for (int num : nums) {
-            for (int i = dp.length - 1; i >= num; i--) {
-                dp[i] = dp[i] || dp[i - num];
+        sum /= 2;
+        boolean[][] dp = new boolean[nums.length][sum + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = true;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            if (j == nums[0]) {
+                dp[0][j] = true;
             }
         }
-        return dp[sum];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (nums[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+        return dp[nums.length - 1][sum];
     }
 
     public static void main(String args[]) {
