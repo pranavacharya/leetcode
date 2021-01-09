665c580a434a868b4dc7373476f7b11d8d12c31f
Pranav Acharya <apranav.acharya@gmail.com>
2021-01-09 17:44:57 +0530

    bfs, TLE

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -1,65 +1,44 @@
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class WordLadder {
 
-    public class Pair {
-
-        String word;
-        int level;
-
-        public Pair(String word, int level) {
-            this.word = word;
-            this.level = level;
-        }
-    }
-
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
-        HashMap<String, ArrayList<String>> generic = new HashMap();
-        for (String word : wordList) {
-            for (int i = 0; i < word.length(); i++) {
-                String before = word.substring(0, i);
-                String after = word.substring(i + 1);
-                String newString = before.concat("*").concat(after);
-                ArrayList<String> combo = generic.getOrDefault(newString, new ArrayList());
-                combo.add(word);
-                generic.put(newString, combo);
-            }
+        if (!wordList.contains(endWord)) {
+            return 0;
         }
-
-        HashMap<String, Boolean> visited = new HashMap();
-        for (String word : wordList) {
-            visited.put(word, false);
-        }
-        Queue<Pair> bfs = new LinkedList<>();
-        bfs.add(new Pair(beginWord, 1));
-        visited.put(beginWord, true);
-        while (!bfs.isEmpty()) {
-            Pair current = bfs.remove();
-            String currentWord = current.word;
-            int currentLevel = current.level;
-            visited.put(currentWord, true);
-            for (int i = 0; i < currentWord.length(); i++) {
-                String before = currentWord.substring(0, i);
-                String after = currentWord.substring(i + 1);
-                String newString = before.concat("*").concat(after);
-                if (generic.containsKey(newString)) {
-                    ArrayList<String> nextList = generic.get(newString);
-                    for (String nxt : nextList) {
-                        if (nxt.equals(endWord)) {
-                            return currentLevel + 1;
-                        }
-                        if (!visited.get(nxt)) {
-                            bfs.add(new Pair(nxt, currentLevel + 1));
+        int ans = 1;
+        Queue<String> queue = new LinkedList();
+        HashSet<String> visited = new HashSet();
+        queue.add(beginWord);
+        visited.add(beginWord);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                String curr = queue.poll();
+                if (curr.equals(endWord)) {
+                    return ans;
+                }
+                char[] arr = curr.toCharArray();
+                for (int i = 0; i < arr.length; i++) {
+                    char c = arr[i];
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        arr[i] = j;
+                        String tempstr = new String(arr);
+                        if (wordList.contains(tempstr) && !visited.contains(tempstr)) {
+                            queue.add(tempstr);
+                            visited.add(tempstr);
                         }
                     }
+                    arr[i] = c;
                 }
+                size--;
             }
+            ans++;
         }
         return 0;
     }
