5a91f9b7bc7f4ee224ed52d9a715b8bfc45d7568
Pranav Acharya <apranav.acharya@gmail.com>
2021-01-10 23:34:03 +0530

    bfs

---
+++ src/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList();
+            while (size > 0) {
+                TreeNode curr = queue.poll();
+                temp.add(curr.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+                size--;
+            }
+            list.add(temp);
+        }
+        return list;
+    }
+
+    public static void main(String args[]) {
+        BinaryTreeLevelOrderTraversal btlot = new BinaryTreeLevelOrderTraversal();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(btlot.levelOrder(root));
+    }
+}
