733f5ba4e336e67dc7d71a37d249b6dd49d4e108
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-01 23:04:19 +0530

    bit manipulation

--- src/NumberOf1Bit.java
+++ src/NumberOf1Bit.java
@@ -2,18 +2,20 @@
 public class NumberOf1Bit {
 
     public int hammingWeight(int n) {
-        String input = Integer.toBinaryString(n);
-        int count = 0;
-        for(char i: input.toCharArray()){
-            if(i == '1'){
-                count++;
+        int weight = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) == mask) {
+                weight++;
             }
+            mask = mask << 1;
         }
-        return count;
+        return weight;
     }
 
     public static void main(String args[]) {
         NumberOf1Bit no1b = new NumberOf1Bit();
         System.out.println(no1b.hammingWeight(11));
+        System.out.println(no1b.hammingWeight(-3));
     }
 }
