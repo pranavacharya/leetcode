c905350162b98b7b9ae49b2b30069a7ffa84f0f6
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-01 23:07:33 +0530

    two pointers

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -1,20 +1,19 @@
 
-import java.util.Arrays;
-
 public class MoveZeroes {
 
     public void moveZeroes(int[] nums) {
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
         while (j < nums.length) {
             if (nums[j] != 0) {
-                int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = temp;
                 i++;
             }
             j++;
         }
-        System.out.println(Arrays.toString(nums));
+        while (i < nums.length) {
+            nums[i++] = 0;
+        }
     }
 
     public static void main(String args[]) {
