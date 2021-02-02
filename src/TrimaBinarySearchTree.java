2f659b4ea376fbddb10c828afa5ac4c3234716a7
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-02 21:53:05 +0530

    recursive solution

--- src/TrimaBinarySearchTree.java
+++ src/TrimaBinarySearchTree.java
@@ -3,13 +3,13 @@ public class TrimaBinarySearchTree {
 
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) {
-            return root;
-        }
-        if (root.val > high) {
-            return trimBST(root.left, low, high);
+            return null;
         }
         if (root.val < low) {
             return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
         }
 
         root.left = trimBST(root.left, low, high);
