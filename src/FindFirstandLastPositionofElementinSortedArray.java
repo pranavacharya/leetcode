f2fc6b3b0da8ccab037e94ba36db475dc936bdf4
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-05 00:56:43 +0530

    binary search, lower & upper bound easy to understand

--- src/FindFirstandLastPositionofElementinSortedArray.java
+++ src/FindFirstandLastPositionofElementinSortedArray.java
@@ -7,31 +7,14 @@ public class FindFirstandLastPositionofElementinSortedArray {
         if (nums.length == 0) {
             return new int[]{-1, -1};
         }
-        int[] ans = new int[2];
-        if (binarySearch(nums, target)) {
-            ans[0] = lowerBound(nums, target);
-            ans[1] = upperBound(nums, target);
-        } else {
-            ans[0] = -1;
-            ans[1] = -1;
+        int[] ans = new int[]{-1, -1};
+        int index = lowerBound(nums, target);
+        if (index == nums.length || nums[index] != target) {
+            return ans;
         }
+        ans[0] = index;
+        ans[1] = upperBound(nums, target) - 1;
         return ans;
-    }
-
-    private boolean binarySearch(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return false;
     }
 
     private int lowerBound(int[] nums, int target) {
@@ -50,21 +33,21 @@ public class FindFirstandLastPositionofElementinSortedArray {
 
     private int upperBound(int[] nums, int target) {
         int low = 0;
-        int high = nums.length - 1;
+        int high = nums.length;
         while (low < high) {
-            int mid = low + (high - low) / 2 + 1;
-            if (nums[mid] > target) {
-                high = mid - 1;
+            int mid = low + (high - low) / 2;
+            if (nums[mid] <= target) {
+                low = mid + 1;
             } else {
-                low = mid;
+                high = mid;
             }
         }
-        return high;
+        return low;
     }
 
     public static void main(String args[]) {
         FindFirstandLastPositionofElementinSortedArray fflpesa = new FindFirstandLastPositionofElementinSortedArray();
         int[] nums = new int[]{5, 7, 7, 8, 8, 10};
-        System.out.println(Arrays.toString(fflpesa.searchRange(nums, 8)));
+        System.out.println(Arrays.toString(fflpesa.searchRange(nums, 10)));
     }
 }
