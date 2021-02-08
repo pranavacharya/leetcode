3760f31c37af6ef6a835b7940f11625cc2909a53
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-09 00:24:59 +0530

    linear solution, two pass

--- src/ShortestDistancetoaCharacter.java
+++ src/ShortestDistancetoaCharacter.java
@@ -5,39 +5,30 @@ public class ShortestDistancetoaCharacter {
 
     public int[] shortestToChar(String S, char C) {
         int[] result = new int[S.length()];
+        Arrays.fill(result, Integer.MAX_VALUE);
+        int distance = Integer.MAX_VALUE;
         for (int i = 0; i < result.length; i++) {
-            result[i] = shortestDistance(i, S, C);
+            if (S.charAt(i) == C) {
+                distance = 0;
+            } else {
+                if (distance != Integer.MAX_VALUE) {
+                    distance++;
+                }
+            }
+            result[i] = distance;
+        }
+        distance = Integer.MAX_VALUE;
+        for (int i = result.length - 1; i >= 0; i--) {
+            if (S.charAt(i) == C) {
+                distance = 0;
+            } else {
+                if (distance != Integer.MAX_VALUE) {
+                    distance++;
+                }
+            }
+            result[i] = Math.min(result[i], distance);
         }
         return result;
-    }
-
-    public int shortestDistance(int index, String s, char c) {
-        int i = index;
-        int j = index;
-        int distance = 0;
-        while (i >= 0 && j < s.length()) {
-            if (s.charAt(i) == c || s.charAt(j) == c) {
-                return distance;
-            }
-            i--;
-            j++;
-            distance++;
-        }
-        while (i >= 0) {
-            if (s.charAt(i) == c) {
-                return distance;
-            }
-            i--;
-            distance++;
-        }
-        while (j < s.length()) {
-            if (s.charAt(j) == c) {
-                return distance;
-            }
-            j++;
-            distance++;
-        }
-        return distance;
     }
 
     public static void main(String args[]) {
