2b9727a3baa09bcf5ce66e205f4b5450dd68ef07
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-10 12:01:09 +0530

    prefix sum, O(N)

--- src/MaximumAbsoluteSumofAnySubarray.java
+++ src/MaximumAbsoluteSumofAnySubarray.java
@@ -3,18 +3,14 @@ public class MaximumAbsoluteSumofAnySubarray {
 
     public int maxAbsoluteSum(int[] nums) {
         int[] prefix = new int[nums.length + 1];
+        int max = 0;
+        int min = 0;
         for (int i = 1; i < prefix.length; i++) {
             prefix[i] = nums[i - 1] + prefix[i - 1];
+            max = Math.max(max, prefix[i]);
+            min = Math.min(min, prefix[i]);
         }
-        int maxSum = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                int sum = prefix[j + 1] - prefix[i];
-                maxSum = Math.max(maxSum, Math.abs(sum));
-            }
-        }
-        return maxSum;
+        return Math.abs(max - min);
     }
 
     public static void main(String args[]) {
