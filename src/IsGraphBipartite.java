9117e7424d2bc770b12b75ed16c597d1624b09e0
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-15 21:23:30 +0530

    bipartite graph

--- src/IsGraphBipartite.java
+++ src/IsGraphBipartite.java
@@ -2,35 +2,36 @@
 public class IsGraphBipartite {
 
     public boolean isBipartite(int[][] graph) {
-        int[] colour = new int[graph.length];
-
-        for (int i = 0; i < colour.length; i++) {
-            if (colour[i] == 0 && !dfs(graph, colour, 1, i)) {
+        int nodes = graph.length;
+        for (int i = 0; i < graph.length; i++) {
+            if (!biparte(graph, new int[nodes], 1, new boolean[nodes], i)) {
                 return false;
             }
         }
-
         return true;
     }
 
-    public boolean dfs(int[][] graph, int[] colour, int currentColour, int node) {
-        if (colour[node] != 0) {
-            return colour[node] == currentColour;
+    private boolean biparte(int[][] graph, int[] colors, int color, boolean[] visited, int node) {
+        if (visited[node]) {
+            if (colors[node] != color) {
+                return false;
+            }
         } else {
-            colour[node] = currentColour;
-            for (int nei : graph[node]) {
-                if (!dfs(graph, colour, -currentColour, nei)) {
+            colors[node] = color;
+            visited[node] = true;
+            for (int neighbour : graph[node]) {
+                if (!biparte(graph, colors, -1 * color, visited, neighbour)) {
                     return false;
                 }
             }
-            return true;
         }
+        return true;
     }
 
     public static void main(String args[]) {
         IsGraphBipartite igb = new IsGraphBipartite();
-//        int[][] graph = new int[][]{{1, 3}, {0, 2}, {1, 3}, {0, 2}};
-        int[][] graph = new int[][]{{1, 2, 3}, {0, 2}, {0, 1, 3}, {0, 2}};
+        int[][] graph = new int[][]{{1, 3}, {0, 2}, {1, 3}, {0, 2}};
+//        int[][] graph = new int[][]{{1, 2, 3}, {0, 2}, {0, 1, 3}, {0, 2}};
         System.out.println(igb.isBipartite(graph));
     }
 }
