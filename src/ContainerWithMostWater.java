483af9cf53aab8b80d3c99c37dfc6c2360ec3f8f
Pranav Acharya <apranav.acharya@gmail.com>
2021-02-17 22:57:14 +0530

    linear solution

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -3,10 +3,15 @@ public class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
         int maxArea = 0;
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i + 1; j < height.length; j++) {
-                int area = Math.min(height[i], height[j]) * (j - i);
-                maxArea = Math.max(maxArea, area);
+        int i = 0;
+        int j = height.length - 1;
+        while (i < j) {
+            int area = Math.min(height[i], height[j]) * (j - i);
+            maxArea = Math.max(maxArea, area);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
             }
         }
         return maxArea;
