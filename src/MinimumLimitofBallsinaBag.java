6a3097711bb5cd957be2d818b89f581531038d28
Pranav Acharya <apranav.acharya@gmail.com>
2021-03-02 17:13:27 +0530

    binary search, mini max search

---
+++ src/MinimumLimitofBallsinaBag.java
@@ -0,0 +1,45 @@
+
+import java.util.Arrays;
+
+public class MinimumLimitofBallsinaBag {
+
+    public int minimumSize(int[] nums, int maxOperations) {
+        Arrays.sort(nums);
+        int low = 0;
+        int high = nums[nums.length - 1];
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canSatisfy(nums, maxOperations, mid)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private boolean canSatisfy(int[] nums, int maxOps, int mid) {
+        if (mid == 0) {
+            return false;
+        }
+        int opsNeeded = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] <= mid) {
+                break;
+            }
+            opsNeeded += calculateDivs(nums[i], mid);
+        }
+        return opsNeeded <= maxOps;
+    }
+
+    private int calculateDivs(int n, int part) {
+        int count = n / part;
+        return n % part == 0 ? count - 1 : count;
+    }
+
+    public static void main(String args[]) {
+        MinimumLimitofBallsinaBag mlobib = new MinimumLimitofBallsinaBag();
+        int[] nums = new int[]{1};
+        System.out.println(mlobib.minimumSize(nums, 1));
+    }
+}
