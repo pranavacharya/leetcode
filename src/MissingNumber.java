b9ad0128b8725838cc48fc63ed5a188f8351840e
Pranav Acharya <apranav.acharya@gmail.com>
2021-03-03 15:34:22 +0530

    O(N) time complexity, O(1) space complexity

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -2,21 +2,32 @@
 public class MissingNumber {
 
     public int missingNumber(int[] nums) {
-        int[] data = new int[nums.length + 1];
-        for (int i : nums) {
-            data[i] = 1;
-        }
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] == 0) {
-                return i;
+        boolean last = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (Math.abs(nums[i]) == nums.length) {
+                last = true;
+            } else {
+                nums[Math.abs(nums[i])] *= -1;
             }
         }
-        return -1;
+        int zeroindex = -1;
+        if (!last) {
+            return nums.length;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > 0) {
+                    return i;
+                } else if (nums[i] == 0) {
+                    zeroindex = i;
+                }
+            }
+        }
+        return zeroindex;
     }
 
     public static void main(String args[]) {
         MissingNumber mn = new MissingNumber();
-        int[] nums = new int[]{3, 0, 1};
+        int[] nums = new int[]{1, 2};
         System.out.println(mn.missingNumber(nums));
     }
 }
