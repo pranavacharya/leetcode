584f06c697dee041e9e36f89afb58a4b40d75b5e
Pranav Acharya <apranav.acharya@gmail.com>
2021-03-11 19:03:38 +0530

    dynamic programing, time complexity (amount * n), space complexity (amount * n )

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -4,17 +4,23 @@ import java.util.Arrays;
 public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], amount + 1);
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                if (j == 0) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                dp[i][j] = dp[i - 1][j];
+                if (coins[i - 1] <= j) {
+                    dp[i][j] = Math.min(dp[i][j], 1 + dp[i][j - coins[i - 1]]);
                 }
             }
         }
-        return (dp[amount] > amount) ? -1 : dp[amount];
+        return dp[coins.length][amount] > amount ? -1 : dp[coins.length][amount];
     }
 
     public static void main(String args[]) {
