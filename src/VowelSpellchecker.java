cc8a68b04ec384dc01c26ef51341b1026a2558b4
Pranav Acharya <apranav.acharya@gmail.com>
2021-03-23 01:45:17 +0530

    hashmap solution, generic string

--- src/VowelSpellchecker.java
+++ src/VowelSpellchecker.java
@@ -1,76 +1,53 @@
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
 
 public class VowelSpellchecker {
 
-    private HashSet<Character> set = new HashSet();
-
-    public VowelSpellchecker() {
-        this.set = new HashSet();
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
-    }
-
-    class Trie {
-
-        List<String> words = new ArrayList();
-        Trie[] childern = new Trie[27];
+    private String createVowelGeneric(String word) {
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                arr[i] = '*';
+            }
+        }
+        return new String(arr);
     }
 
     public String[] spellchecker(String[] wordlist, String[] queries) {
-        Trie root = buildTrie(wordlist);
+        HashMap<String, String> original = new HashMap();
+        HashMap<String, String> cap = new HashMap();
+        HashMap<String, String> vowel = new HashMap();
+
+        for (String word : wordlist) {
+            if (!original.containsKey(word)) {
+                original.put(word, word);
+            }
+
+            String lower = word.toLowerCase();
+            if (!cap.containsKey(lower)) {
+                cap.put(lower, word);
+            }
+
+            String vowelgen = createVowelGeneric(word.toLowerCase());
+            if (!vowel.containsKey(vowelgen)) {
+                vowel.put(vowelgen, word);
+            }
+        }
         String[] res = new String[queries.length];
         for (int i = 0; i < queries.length; i++) {
-            res[i] = findWord(root, queries[i]);
+            if (original.containsKey(queries[i])) {
+                res[i] = original.get(queries[i]);
+
+            } else if (cap.containsKey(queries[i].toLowerCase())) {
+                res[i] = cap.get(queries[i].toLowerCase());
+            } else if (vowel.containsKey(createVowelGeneric(queries[i].toLowerCase()))) {
+                res[i] = vowel.get(createVowelGeneric(queries[i].toLowerCase()));
+            } else {
+                res[i] = "";
+            }
         }
         return res;
-    }
-
-    private Trie buildTrie(String[] wordlist) {
-        Trie root = new Trie();
-        for (String word : wordlist) {
-            Trie temp = root;
-            for (int j = 0; j < word.length(); j++) {
-                char c = Character.toLowerCase(word.charAt(j));
-                if (this.set.contains(c)) {
-                    // char after z in ascii
-                    c = '{';
-                }
-                if (temp.childern[c - 'a'] == null) {
-                    temp.childern[c - 'a'] = new Trie();
-                }
-                temp = temp.childern[c - 'a'];
-            }
-            temp.words.add(word);
-        }
-        return root;
-    }
-
-    private String findWord(Trie root, String word) {
-        Trie temp = root;
-        for (int i = 0; i < word.length(); i++) {
-            char c = Character.toLowerCase(word.charAt(i));
-            if (this.set.contains(c)) {
-                // char after z in ascii
-                c = '{';
-            }
-            if (temp.childern[c - 'a'] == null) {
-                return "";
-            } else {
-                temp = temp.childern[c - 'a'];
-            }
-        }
-        List<String> words = temp.words;
-        if (words.contains(word)) {
-            return word;
-        }
-        return words.isEmpty() ? "" : words.get(0);
     }
 
     public static void main(String[] args) {
