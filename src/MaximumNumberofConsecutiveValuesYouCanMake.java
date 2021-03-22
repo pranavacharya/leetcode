04868906bef28bbd38cd8e540445d923fd6c6813
Pranav Acharya <apranav.acharya@gmail.com>
2021-03-22 23:28:17 +0530

    greedy solution, time complexity O(N)

--- src/MaximumNumberofConsecutiveValuesYouCanMake.java
+++ src/MaximumNumberofConsecutiveValuesYouCanMake.java
@@ -1,48 +1,24 @@
 
+import java.util.Arrays;
+
 public class MaximumNumberofConsecutiveValuesYouCanMake {
 
     public int getMaximumConsecutive(int[] coins) {
-        int n = coins.length;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += coins[i];
-        }
-        int[][] dp = new int[2][sum + 1];
-        for (int i = 0; i < 2; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (i % 2 == 0) {
-                    dp[1][j] = dp[0][j];
-                    if (j >= coins[i]) {
-                        dp[1][j] += dp[0][j - coins[i]];
-                    }
-                } else {
-                    dp[0][j] = dp[1][j];
-                    if (j >= coins[i]) {
-                        dp[0][j] += dp[1][j - coins[i]];
-                    }
-                }
-            }
-        }
-        int[] valid = coins.length % 2 == 0 ? dp[0] : dp[1];
         int ans = 0;
-        int count = 0;
-        for (int i = 0; i < valid.length; i++) {
-            if (valid[i] != 0) {
-                count++;
+        Arrays.sort(coins);
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] > ans + 1) {
+                break;
             } else {
-                count = 0;
+                ans += coins[i];
             }
-            ans = Math.max(ans, count);
         }
-        return ans;
+        return ans + 1;
     }
 
     public static void main(String[] args) {
         MaximumNumberofConsecutiveValuesYouCanMake mncvym = new MaximumNumberofConsecutiveValuesYouCanMake();
-        int[] coins = new int[]{1, 4, 10, 3, 1};
+        int[] coins = new int[]{1, 3};
         System.out.println(mncvym.getMaximumConsecutive(coins));
     }
 }
