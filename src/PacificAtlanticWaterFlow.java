c54437278c998f098c400a15de0a51f08e743dbf
Pranav Acharya <apranav.acharya@gmail.com>
2021-03-25 22:15:34 +0530

    bfs solution

--- src/PacificAtlanticWaterFlow.java
+++ src/PacificAtlanticWaterFlow.java
@@ -1,6 +1,8 @@
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class PacificAtlanticWaterFlow {
 
@@ -14,13 +16,12 @@ public class PacificAtlanticWaterFlow {
         int n = matrix[0].length;
         boolean[][] visitedPacific = new boolean[m][n];
         boolean[][] visitedAtalantic = new boolean[m][n];
-        boolean[][] isValidPacific = new boolean[m][n];
-        boolean[][] isValidAtlantic = new boolean[m][n];
+        bfs(matrix, visitedPacific, new boolean[m][n], true, m, n);
+        bfs(matrix, visitedAtalantic, new boolean[m][n], false, m, n);
         List<List<Integer>> result = new ArrayList();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (visitAtlanticFromNode(matrix, visitedAtalantic, isValidAtlantic, new boolean[m][n], i, j, m, n)
-                        && visitPacficFromNode(matrix, visitedPacific, isValidPacific, new boolean[m][n], i, j, m, n)) {
+                if (visitedAtalantic[i][j] && visitedPacific[i][j]) {
                     List<Integer> point = new ArrayList();
                     point.add(i);
                     point.add(j);
@@ -31,87 +32,41 @@ public class PacificAtlanticWaterFlow {
         return result;
     }
 
-    private boolean visitPacficFromNode(int[][] matrix, boolean[][] visited,
-            boolean[][] isValid, boolean[][] visiting, int row, int col, int m, int n) {
-
-        if (row < 0 || row >= m || col < 0 || col >= n) {
-            return false;
-        }
-
-        if (visited[row][col]) {
-            return isValid[row][col];
-        }
-
-        boolean isPacific = false;
-        if (row == 0 || col == 0) {
-            isPacific = true;
-        }
-
-        if (isPacific) {
-            visited[row][col] = true;
-            isValid[row][col] = isPacific;
-            return isValid[row][col];
-        }
-
-        visiting[row][col] = true;
-        for (int[] dir : dirs) {
-            if (row + dir[0] < 0 || row + dir[0] >= m || col + dir[1] < 0 || col + dir[1] >= n) {
-                continue;
-            }
-            if (matrix[row][col] >= matrix[row + dir[0]][col + dir[1]]
-                    && !visiting[row + dir[0]][col + dir[1]]
-                    && visitPacficFromNode(matrix, visited, isValid, visiting, row + dir[0], col + dir[1], m, n)) {
-                isPacific = true;
-                break;
+    private void bfs(int[][] matrix, boolean[][] canReach,
+            boolean[][] visited, boolean isPacific, int m, int n) {
+        Queue<int[]> queue = new LinkedList();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isPacific) {
+                    if (i == 0 || j == 0) {
+                        visited[i][j] = true;
+                        queue.add(new int[]{i, j});
+                    }
+                } else {
+                    if (i == m - 1 || j == n - 1) {
+                        visited[i][j] = true;
+                        queue.add(new int[]{i, j});
+                    }
+                }
             }
         }
-        visiting[row][col] = false;
-        if (isPacific) {
-            visited[row][col] = true;
-        }
-        isValid[row][col] = isPacific;
-        return isValid[row][col];
-    }
 
-    private boolean visitAtlanticFromNode(int[][] matrix, boolean[][] visited,
-            boolean[][] isValid, boolean[][] visiting, int row, int col, int m, int n) {
-
-        if (row < 0 || row >= m || col < 0 || col >= n) {
-            return false;
-        }
-
-        if (visited[row][col]) {
-            return isValid[row][col];
-        }
-
-        boolean isAtlantic = false;
-
-        if (row == m - 1 || col == n - 1) {
-            isAtlantic = true;
-        }
-        if (isAtlantic) {
-            visited[row][col] = true;
-            isValid[row][col] = isAtlantic;
-            return isValid[row][col];
-        }
-        visiting[row][col] = true;
-        for (int[] dir : dirs) {
-            if (row + dir[0] < 0 || row + dir[0] >= m || col + dir[1] < 0 || col + dir[1] >= n) {
-                continue;
-            }
-            if (matrix[row][col] >= matrix[row + dir[0]][col + dir[1]]
-                    && !visiting[row + dir[0]][col + dir[1]]
-                    && visitAtlanticFromNode(matrix, visited, isValid, visiting, row + dir[0], col + dir[1], m, n)) {
-                isAtlantic = true;
-                break;
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0];
+            int col = curr[1];
+            canReach[curr[0]][curr[1]] = true;
+            for (int[] dir : dirs) {
+                if (row + dir[0] >= 0 && row + dir[0] < m && col + dir[1] >= 0
+                        && col + dir[1] < n
+                        && !visited[row + dir[0]][col + dir[1]]
+                        && matrix[row][col] <= matrix[row + dir[0]][col + dir[1]]) {
+                    visited[row + dir[0]][col + dir[1]] = true;
+                    queue.add(new int[]{row + dir[0], col + dir[1]});
+                }
             }
         }
-        visiting[row][col] = false;
-        if (isAtlantic) {
-            visited[row][col] = true;
-        }
-        isValid[row][col] = isAtlantic;
-        return isValid[row][col];
+
     }
 
     public static void main(String[] args) {
