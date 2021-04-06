f5edd9271be3f16d2fcf9be0dcb710265437a99c
Pranav Acharya <apranav.acharya@gmail.com>
2021-04-06 21:33:12 +0530

    math solution, time complexity O(N) space complexity O(1)

--- src/MinimumOperationstoMakeArrayEqual.java
+++ src/MinimumOperationstoMakeArrayEqual.java
@@ -2,15 +2,11 @@
 public class MinimumOperationstoMakeArrayEqual {
 
     public int minOperations(int n) {
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i * 2 + 1;
+        int ops = 0;
+        for (int i = 0; i < n / 2; i++) {
+            ops += (n - ((2 * i) + 1));
         }
-        int count = 0;
-        for (int i = 0; i < (n + 1) / 2; i++) {
-            count += n - arr[i];
-        }
-        return count;
+        return ops;
     }
 
     public static void main(String args[]) {
