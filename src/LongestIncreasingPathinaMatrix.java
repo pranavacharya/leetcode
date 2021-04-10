c48800bc439bd3812639d32ee7ba283f78d2617f
Pranav Acharya <apranav.acharya@gmail.com>
2021-04-10 23:52:42 +0530

    dfs + dynamic programming (memorization)

--- src/LongestIncreasingPathinaMatrix.java
+++ src/LongestIncreasingPathinaMatrix.java
@@ -1,35 +1,40 @@
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LongestIncreasingPathinaMatrix {
 
     private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-    int longestPath = 0;
+    int[][] dp;
 
     public int longestIncreasingPath(int[][] matrix) {
+        this.dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        int longestPath = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                dfs(matrix, i, j, new ArrayList());
+                longestPath = Math.max(longestPath, dfs(matrix, i, j));
             }
         }
         return longestPath;
     }
 
-    private void dfs(int[][] matrix, int row, int col, ArrayList<Integer> path) {
-        this.longestPath = Math.max(this.longestPath, path.size() + 1);
-
+    private int dfs(int[][] matrix, int row, int col) {
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        int pathLength = 1;
+        int maxLength = 0;
         for (int[] dir : dirs) {
-
             if (row + dir[0] >= 0 && row + dir[0] < matrix.length && col + dir[1] >= 0
                     && col + dir[1] < matrix[0].length
                     && matrix[row + dir[0]][col + dir[1]] > matrix[row][col]) {
-                path.add(matrix[row][col]);
-                dfs(matrix, row + dir[0], col + dir[1], path);
-                path.remove(path.size() - 1);
+                maxLength = Math.max(maxLength, dfs(matrix, row + dir[0], col + dir[1]));
             }
         }
-
+        this.dp[row][col] = pathLength + maxLength;
+        return this.dp[row][col];
     }
 
     public static void main(String[] args) {
