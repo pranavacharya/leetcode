a4492222f2151f0a8826b69122e6437611543bdf
Pranav Acharya <apranav.acharya@gmail.com>
2021-04-19 23:22:33 +0530

    dynamic programming, top down approach

--- src/CombinationSumIV.java
+++ src/CombinationSumIV.java
@@ -1,27 +1,35 @@
 
+import java.util.Arrays;
+
 public class CombinationSumIV {
 
-    private int count = 0;
+    private int[] dp;
 
     public int combinationSum4(int[] nums, int target) {
-        backtracking(nums, target);
-        return this.count;
+        this.dp = new int[target + 1];
+        Arrays.fill(this.dp, -1);
+        return backtracking(nums, target);
     }
 
-    private void backtracking(int[] nums, int target) {
-        if (target == 0) {
-            this.count++;
+    private int backtracking(int[] nums, int target) {
+        if (this.dp[target] != -1) {
+            return this.dp[target];
         }
+        if (target == 0) {
+            return this.dp[target] = 1;
+        }
+        int ans = 0;
         for (int i = 0; i < nums.length; i++) {
             if (target >= nums[i]) {
-                backtracking(nums, target - nums[i]);
+                ans += backtracking(nums, target - nums[i]);
             }
         }
+        return this.dp[target] = ans;
     }
 
     public static void main(String[] args) {
         CombinationSumIV cs = new CombinationSumIV();
-        int[] nums = new int[]{1, 2, 3};
-        System.out.println(cs.combinationSum4(nums, 4));
+        int[] nums = new int[]{2, 1, 3};
+        System.out.println(cs.combinationSum4(nums, 35));
     }
 }
