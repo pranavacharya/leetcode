8bb372229f0ae22e82654821bf08d69e05c8d3a0
Pranav Acharya <apranav.acharya@gmail.com>
2021-04-23 21:26:42 +0530

    dynamic programming, top down approach - 1D

--- src/PartitionArrayforMaximumSum.java
+++ src/PartitionArrayforMaximumSum.java
@@ -3,40 +3,25 @@ import java.util.Arrays;
 
 public class PartitionArrayforMaximumSum {
 
-    private int[][] dp;
+    private int[] dp;
 
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        this.dp = new int[arr.length][arr.length];
-        for (int i = 0; i < this.dp.length; i++) {
-            Arrays.fill(this.dp[i], -1);
-        }
-        return helper(0, arr.length - 1, arr, k);
+        this.dp = new int[arr.length + 1];
+        Arrays.fill(this.dp, -1);
+        return helper(0, arr, k);
     }
 
-    private int helper(int i, int j, int[] arr, int min) {
-        if (this.dp[i][j] != -1) {
-            return this.dp[i][j];
+    private int helper(int i, int[] arr, int min) {
+        if (this.dp[i] != -1) {
+            return this.dp[i];
         }
-
-        if (i == j) {
-            return this.dp[i][j] = arr[i];
-        }
-
-        if (j - i + 1 <= min) {
-            int max = 0;
-            for (int k = i; k <= j; k++) {
-                max = Math.max(max, arr[k]);
-            }
-            return this.dp[i][j] = max * (j - i + 1);
-        }
-
         int max = 0;
-
-        for (int k = i; k < j; k++) {
-            max = Math.max(max, helper(i, k, arr, min) + helper(k + 1, j, arr, min));
+        int maxElement = 0;
+        for (int k = i; k < Math.min(arr.length, i + min); k++) {
+            maxElement = Math.max(maxElement, arr[k]);
+            max = Math.max(max, maxElement * (k - i + 1) + helper(k + 1, arr, min));
         }
-
-        return this.dp[i][j] = max;
+        return this.dp[i] = max;
     }
 
     public static void main(String[] args) {
