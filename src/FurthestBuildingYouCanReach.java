444823d31c3aad1c74ad622d2d3ca5751e6a6378
Pranav Acharya <apranav.acharya@gmail.com>
2021-04-27 00:03:35 +0530

    greedy solution, time complexity O(NlogN)

--- src/FurthestBuildingYouCanReach.java
+++ src/FurthestBuildingYouCanReach.java
@@ -4,18 +4,25 @@ import java.util.PriorityQueue;
 public class FurthestBuildingYouCanReach {
 
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        PriorityQueue<Integer> minHeap
-                = new PriorityQueue<>();
-        for (int i = 0; i < heights.length - 1; i++) {
-            int diff = heights[i + 1] - heights[i];
-            if (diff > 0) {
-                minHeap.add(diff);
-            }
-            if (minHeap.size() > ladders) {
-                bricks -= minHeap.poll();
-            }
-            if (bricks < 0) {
-                return i;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> (b - a));
+        for (int i = 1; i < heights.length; i++) {
+            int bricksNeeded = heights[i] - heights[i - 1];
+            if (bricksNeeded > 0) {
+                if (bricks < bricksNeeded) {
+                    if (ladders <= 0) {
+                        return i - 1;
+                    } else {
+                        if (!maxHeap.isEmpty() && maxHeap.peek() > bricksNeeded) {
+                            bricks += maxHeap.poll();
+                            maxHeap.add(bricksNeeded);
+                            bricks -= bricksNeeded;
+                        }
+                        ladders--;
+                    }
+                } else {
+                    maxHeap.add(bricksNeeded);
+                    bricks -= bricksNeeded;
+                }
             }
         }
         return heights.length - 1;
