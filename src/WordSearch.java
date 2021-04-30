8024e8b33f3fe240f817b5977f825fa7f8d0cb42
Pranav Acharya <apranav.acharya@gmail.com>
2021-04-30 22:51:10 +0530

    dfs

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,41 +1,39 @@
 
 public class WordSearch {
 
-    private int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (dfs(board, word, 0, i, j)) {
-                        return true;
-                    }
+                if (board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0)) {
+                    return true;
                 }
             }
         }
         return false;
     }
 
-    public boolean dfs(char[][] board, String word, int index, int row, int col) {
+    private boolean dfs(char[][] board, int i, int j, String word, int index) {
         if (index == word.length()) {
             return true;
-        } else if (row < 0 || col < 0
-                || row >= board.length || col >= board[row].length) {
-            return false;
-        } else if (board[row][col] == word.charAt(index)) {
-            for (int[] d : dir) {
-                char temp = board[row][col];
-                board[row][col] = '1';
-                if (dfs(board, word, index + 1, row + d[0], col + d[1])) {
-                    return true;
-                } else {
-                    board[row][col] = temp;
-                }
-            }
-            return false;
-        } else {
+        }
+        if (i >= board.length || i < 0
+                || j >= board[i].length || j < 0) {
             return false;
         }
+        if (word.charAt(index) != board[i][j]) {
+            return false;
+        }
+        char value = board[i][j];
+        board[i][j] = '#';
+        for (int[] dir : dirs) {
+            if (dfs(board, i + dir[0], j + dir[1], word, index + 1)) {
+                return true;
+            }
+        }
+        board[i][j] = value;
+        return false;
     }
 
     public static void main(String args[]) {
