d25c41b27b806d3e46af2afe8a6662ba6c9c3a06
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-02 02:18:10 +0530

    trie solution, words modified in suffix substrings + prefix form

--- src/PrefixandSuffixSearch.java
+++ src/PrefixandSuffixSearch.java
@@ -1,80 +1,55 @@
 
-import java.util.HashSet;
-
 public class PrefixandSuffixSearch {
 
     class Trie {
 
-        Trie[] children = new Trie[26];
+        Trie[] children = new Trie[27];
         boolean isWord = false;
-        HashSet<Integer> set = new HashSet();
+        int index = 0;
     }
 
-    private Trie prefixTrie;
-    private Trie suffixTrie;
+    private Trie root;
 
-    private void buildTrie(Trie root, String[] words, int order) {
+    private void buildTrie(Trie root, String[] words) {
         for (int i = 0; i < words.length; i++) {
-            Trie temp = root;
-            String word = words[i];
-            if (order == 1) {
-                for (int j = 0; j < word.length(); j++) {
-                    char c = word.charAt(j);
+            String word = words[i] + "{";
+            for (int j = 0; j < word.length(); j++) {
+                Trie temp = root;
+                temp.index = i;
+                for (int k = j; k < 2 * word.length() - 1; k++) {
+                    char c = word.charAt(k % word.length());
                     if (temp.children[c - 'a'] == null) {
                         temp.children[c - 'a'] = new Trie();
                     }
                     temp = temp.children[c - 'a'];
-                    temp.set.add(i);
-                }
-                temp.isWord = true;
-            } else {
-                for (int j = word.length() - 1; j >= 0; j--) {
-                    char c = word.charAt(j);
-                    if (temp.children[c - 'a'] == null) {
-                        temp.children[c - 'a'] = new Trie();
-                    }
-                    temp = temp.children[c - 'a'];
-                    temp.set.add(i);
+                    temp.index = i;
                 }
                 temp.isWord = true;
             }
         }
     }
 
-    private HashSet<Integer> searchTrie(Trie root, String prefix) {
-        HashSet<Integer> ids = new HashSet();
-        Trie temp = root;
-        for (int i = 0; i < prefix.length(); i++) {
-            char c = prefix.charAt(i);
+    private int searchTrie(String word) {
+        Trie temp = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
             if (temp.children[c - 'a'] == null) {
-                return new HashSet();
+                return -1;
             } else {
                 temp = temp.children[c - 'a'];
-                ids = new HashSet(temp.set);
             }
         }
-        return ids;
+        return temp.index;
     }
 
     public PrefixandSuffixSearch(String[] words) {
-        this.prefixTrie = new Trie();
-        this.suffixTrie = new Trie();
-        buildTrie(prefixTrie, words, 1);
-        buildTrie(suffixTrie, words, 0);
+        this.root = new Trie();
+        buildTrie(root, words);
     }
 
     public int f(String prefix, String suffix) {
-        HashSet<Integer> prefixSet = searchTrie(prefixTrie, prefix);
-        StringBuilder sb = new StringBuilder();
-        sb.append(suffix);
-        String revSuff = sb.reverse().toString();
-        HashSet<Integer> suffixSet = searchTrie(suffixTrie, revSuff);
-        prefixSet.retainAll(suffixSet);
-        int max = -1;
-        for (int index : prefixSet) {
-            max = Math.max(max, index);
-        }
-        return max;
+        String word = suffix + "{" + prefix;
+        return searchTrie(word);
     }
 
     public static void main(String[] args) {
