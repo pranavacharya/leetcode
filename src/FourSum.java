e11cc07e37560386366828fecec147d6f4ae5d07
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-06 23:46:43 +0530

    three sum + sum, time complexity O(N ^ 3)

--- src/FourSum.java
+++ src/FourSum.java
@@ -8,41 +8,52 @@ public class FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 3; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            for (int j = i + 1; j < nums.length; j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (k != j + 1 && nums[k] == nums[k - 1]) {
-                        continue;
-                    }
-                    for (int l = k + 1; l < nums.length; l++) {
-                        if (l != k + 1 && nums[l] == nums[l - 1]) {
-                            continue;
-                        }
-                        if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
-                            List<Integer> temp = new ArrayList();
-                            temp.add(nums[i]);
-                            temp.add(nums[j]);
-                            temp.add(nums[k]);
-                            temp.add(nums[l]);
-                            result.add(temp);
-                        }
-                    }
+            result.addAll(threeSum(nums, i + 1, target - nums[i], nums[i]));
+        }
+        return result;
+    }
+
+    private List<List<Integer>> threeSum(int[] nums, int index, int target, int first) {
+        List<List<Integer>> list = new ArrayList();
+        for (int k = index; k < nums.length - 2; k++) {
+            if (k != index && nums[k] == nums[k - 1]) {
+                continue;
+            }
+            int newTarget = target - nums[k];
+            int i = k + 1;
+            int j = nums.length - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] == newTarget) {
+                    List<Integer> temp = new ArrayList();
+                    temp.add(first);
+                    temp.add(nums[k]);
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    list.add(temp);
+                    do {
+                        i++;
+                    } while (i < nums.length && nums[i] == nums[i - 1]);
+                    do {
+                        j--;
+                    } while (j >= 0 && nums[j] == nums[j + 1]);
+                } else if (nums[i] + nums[j] < newTarget) {
+                    i++;
+                } else {
+                    j--;
                 }
             }
         }
 
-        return result;
+        return list;
     }
 
     public static void main(String[] args) {
         FourSum fs = new FourSum();
-        int[] nums = new int[]{2, 2, 2, 2, 2};
-        System.out.println(fs.fourSum(nums, 8));
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
+        System.out.println(fs.fourSum(nums, 0));
     }
 }
