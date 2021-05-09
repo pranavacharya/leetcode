e1cadf47b116260df5bf56e08bdd96a6b64752b5
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-09 18:01:57 +0530

    brute force + prefix array, TLE

--- src/MaximumSubarrayMinProduct.java
+++ src/MaximumSubarrayMinProduct.java
@@ -1,32 +1,22 @@
 
-import java.util.PriorityQueue;
-
 public class MaximumSubarrayMinProduct {
 
     int mod = (int) 1e9 + 7;
 
     public int maxSumMinProduct(int[] nums) {
-        int[] prefix = new int[nums.length + 1];
+        long[] prefix = new long[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
             prefix[i + 1] = prefix[i] + nums[i];
         }
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> (nums[a] - nums[b]));
 
         long maxMinProduct = 0;
-        for (int length = 1; length <= nums.length; length++) {
-            minHeap.clear();
-            for (int i = 0; i < nums.length; i++) {
-                minHeap.add(i);
-                while (!minHeap.isEmpty() && minHeap.peek() <= i - length) {
-                    minHeap.poll();
-                }
-                int start = i - length + 1;
-                int end = i;
-                if (start >= 0) {
-                    long sum = prefix[end + 1] - prefix[start];
-                    long product = sum * nums[minHeap.peek()];
-                    maxMinProduct = Math.max(maxMinProduct, product);
-                }
+        for (int i = 0; i < nums.length; i++) {
+            int min = nums[i];
+            for (int j = i; j < nums.length; j++) {
+                min = Math.min(min, nums[j]);
+                long sum = prefix[j + 1] - prefix[i];
+                long product = sum * min;
+                maxMinProduct = Math.max(maxMinProduct, product);
             }
         }
         return (int) (maxMinProduct % mod);
