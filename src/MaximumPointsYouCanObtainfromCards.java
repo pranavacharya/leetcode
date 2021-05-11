125caf679e2b3ada0c15d858c0d533d5ba186ff5
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-11 23:42:50 +0530

    sliding window, time complexity O(N)

--- src/MaximumPointsYouCanObtainfromCards.java
+++ src/MaximumPointsYouCanObtainfromCards.java
@@ -1,28 +1,24 @@
 
-import java.util.Arrays;
-
 public class MaximumPointsYouCanObtainfromCards {
 
-    private int[][][] dp;
-
     public int maxScore(int[] cardPoints, int k) {
-        this.dp = new int[cardPoints.length][cardPoints.length][k + 1];
-        for (int i = 0; i < this.dp.length; i++) {
-            for (int j = 0; j < this.dp[i].length; j++) {
-                Arrays.fill(this.dp[i][j], -1);
+        int[] modified = new int[2 * cardPoints.length];
+        for (int i = 0; i < cardPoints.length; i++) {
+            modified[i] = cardPoints[i];
+            modified[i + cardPoints.length] = cardPoints[i];
+        }
+        int start = cardPoints.length - k;
+        int end = start;
+        int ans = 0;
+        int sum = 0;
+        while (start <= cardPoints.length) {
+            while (end - start < k) {
+                sum += modified[end++];
             }
+            ans = Math.max(ans, sum);
+            sum -= modified[start++];
         }
-        return helper(0, cardPoints.length - 1, k, cardPoints);
-    }
-
-    private int helper(int i, int j, int k, int[] cardPoints) {
-        if (k == 0) {
-            return 0;
-        }
-        if (this.dp[i][j][k] != -1) {
-            return this.dp[i][j][k];
-        }
-        return this.dp[i][j][k] = Math.max(helper(i + 1, j, k - 1, cardPoints) + cardPoints[i], helper(i, j - 1, k - 1, cardPoints) + cardPoints[j]);
+        return ans;
     }
 
     public static void main(String args[]) {
