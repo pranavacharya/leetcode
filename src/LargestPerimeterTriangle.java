76c059dbe51ac113f2a083f484d6027c03c25f03
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-14 23:29:24 +0530

    sorting and linear scan, time complexity O(NlogN)

--- src/LargestPerimeterTriangle.java
+++ src/LargestPerimeterTriangle.java
@@ -5,13 +5,9 @@ public class LargestPerimeterTriangle {
 
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        for (int i = nums.length - 1; i >= 0; i--) {
-            for (int j = i - 1; j >= 0; j--) {
-                for (int k = j - 1; k >= 0; k--) {
-                    if (nums[k] + nums[j] > nums[i]) {
-                        return nums[k] + nums[j] + nums[i];
-                    }
-                }
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i] < nums[i - 1] + nums[i - 2]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
             }
         }
         return 0;
