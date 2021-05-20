10dbfe28bb92b43ce5d4787b9cf79769b38fcd50
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-20 23:30:09 +0530

    bfs

--- src/BinaryTreeLevelOrderTraversal.java
+++ src/BinaryTreeLevelOrderTraversal.java
@@ -7,29 +7,29 @@ import java.util.Queue;
 public class BinaryTreeLevelOrderTraversal {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList();
-        if (root == null) {
-            return list;
-        }
+        List<List<Integer>> result = new ArrayList();
         Queue<TreeNode> queue = new LinkedList();
+        if (root == null) {
+            return result;
+        }
         queue.add(root);
         while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList();
             int size = queue.size();
-            List<Integer> temp = new ArrayList();
             while (size > 0) {
                 TreeNode curr = queue.poll();
-                temp.add(curr.val);
                 if (curr.left != null) {
                     queue.add(curr.left);
                 }
                 if (curr.right != null) {
                     queue.add(curr.right);
                 }
+                list.add(curr.val);
                 size--;
             }
-            list.add(temp);
+            result.add(list);
         }
-        return list;
+        return result;
     }
 
     public static void main(String args[]) {
