5371afd8313e7f40ff774e37ab03542ff2a5ff65
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-28 22:31:33 +0530

    dfs + binary search, TLE

--- src/PathWithMinimumEffort.java
+++ src/PathWithMinimumEffort.java
@@ -3,33 +3,39 @@ public class PathWithMinimumEffort {
 
     private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    private int min;
-
     public int minimumEffortPath(int[][] heights) {
-        this.min = Integer.MAX_VALUE;
-        dfs(0, 0, heights, new boolean[heights.length][heights[0].length], 0);
-        return this.min;
+        int low = 0;
+        int high = (int) 1e6;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (dfs(0, 0, heights, new boolean[heights.length][heights[0].length], mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return high;
     }
 
-    private void dfs(int row, int col, int[][] heights, boolean[][] visited, int sum) {
-        if (sum > this.min) {
-            return;
-        }
+    private boolean dfs(int row, int col, int[][] heights, boolean[][] visited, int limit) {
         if (row == heights.length - 1 && col == heights[row].length - 1) {
-            min = Math.min(min, sum);
+            return true;
         }
         for (int[] dir : dirs) {
             int row1 = row + dir[0];
             int col1 = col + dir[1];
             if (row1 >= 0 && row1 < heights.length
                     && col1 >= 0 && col1 < heights[row1].length
-                    && !visited[row1][col1]) {
+                    && !visited[row1][col1] && Math.abs(heights[row][col] - heights[row1][col1]) <= limit) {
                 visited[row1][col1] = true;
-                dfs(row1, col1, heights, visited,
-                        Math.max(sum, Math.abs(heights[row][col] - heights[row1][col1])));
+                if (dfs(row1, col1, heights, visited, limit)) {
+                    visited[row1][col1] = false;
+                    return true;
+                }
                 visited[row1][col1] = false;
             }
         }
+        return false;
     }
 
     public static void main(String[] args) {
