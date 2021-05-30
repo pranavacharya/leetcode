e9d58e69d8c609ca498396271218cbd82a04fc09
Pranav Acharya <apranav.acharya@gmail.com>
2021-05-30 23:53:22 +0530

    linear solution, radix sort, time complexity O(N), space complexity O(N)

--- src/MaximumGap.java
+++ src/MaximumGap.java
@@ -1,13 +1,42 @@
 
-import java.util.Arrays;
-
 public class MaximumGap {
 
     public int maximumGap(int[] nums) {
-        Arrays.sort(nums);
-        int ans = 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        // Radix sort
+        int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            ans = Math.max(ans, nums[i] - nums[i - 1]);
+            max = Math.max(max, nums[i]);
+        }
+        int exp = 1;
+        int R = 10;
+        int[] aux = new int[nums.length];
+        while (max / exp > 0) {
+            int[] count = new int[R];
+
+            for (int i = 0; i < nums.length; i++) {
+                count[(nums[i] / exp) % 10]++;
+            }
+
+            for (int i = 1; i < count.length; i++) {
+                count[i] += count[i - 1];
+            }
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                aux[--count[(nums[i] / exp) % 10]] = nums[i];
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = aux[i];
+            }
+            exp *= 10;
+        }
+
+        int ans = 0;
+        for (int i = 1; i < aux.length; i++) {
+            ans = Math.max(ans, aux[i] - aux[i - 1]);
         }
         return ans;
     }
