2d364bbcd7943328c7bf34628eab616838340e14
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-01 23:24:53 +0530

    dfs solution

--- src/MaxAreaOfIsland.java
+++ src/MaxAreaOfIsland.java
@@ -1,38 +1,35 @@
 
 public class MaxAreaOfIsland {
 
-    private int[][] grid;
+    private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    private int maxArea;
 
     public int maxAreaOfIsland(int[][] grid) {
-        this.grid = grid;
-        int maxArea = 0;
-        for (int i = 0; i < this.grid.length; i++) {
-            for (int j = 0; j < this.grid[i].length; j++) {
-                if (this.grid[i][j] == 1) {
-                    int localArea = dfs(i, j);
-                    maxArea = Math.max(maxArea, localArea);
+        this.maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, helper(grid, i, j));
                 }
             }
         }
-        return maxArea;
+        return this.maxArea;
     }
 
-    public int dfs(int i, int j) {
-        if (i >= 0 && i < this.grid.length && j >= 0 && j < this.grid[i].length) {
-            if (this.grid[i][j] == 1) {
-                this.grid[i][j] = 0;
-                int ans = 1;
-                ans += dfs(i + 1, j);
-                ans += dfs(i, j + 1);
-                ans += dfs(i - 1, j);
-                ans += dfs(i, j - 1);
-                return ans;
-            } else {
-                return 0;
+    private int helper(int[][] grid, int row, int col) {
+        int area = 1;
+        grid[row][col] = 0;
+        for (int[] dir : dirs) {
+            int row1 = row + dir[0];
+            int col1 = col + dir[1];
+            if (row1 >= 0 && row1 < grid.length
+                    && col1 >= 0 && col1 < grid[row1].length
+                    && grid[row1][col1] == 1) {
+                area += helper(grid, row1, col1);
             }
-        } else {
-            return 0;
         }
+        return area;
     }
 
     public static void main(String args[]) {
