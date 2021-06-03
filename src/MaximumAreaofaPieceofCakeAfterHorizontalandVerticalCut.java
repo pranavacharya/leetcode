e0645de51b18d238c1fdb36f8c1085761caa62a9
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-03 23:33:41 +0530

    greedy solution, time complexity O(NlogN)

--- src/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCut.java
+++ src/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCut.java
@@ -3,38 +3,39 @@ import java.util.Arrays;
 
 public class MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCut {
 
+    private int mod = (int) 1e9 + 7;
+
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        int maxArea = 0;
-        int hLast = 0;
         Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
+        long maxHorDiff = 0;
         for (int i = 0; i <= horizontalCuts.length; i++) {
-            int height = 0;
-            if (i == horizontalCuts.length) {
-                height = h - hLast;
+            if (i == 0) {
+                maxHorDiff = Math.max(maxHorDiff, horizontalCuts[i] - 0);
+            } else if (i == horizontalCuts.length) {
+                maxHorDiff = Math.max(maxHorDiff, h - horizontalCuts[i - 1]);
             } else {
-                height = horizontalCuts[i] - hLast;
-            }
-            int vLast = 0;
-            for (int j = 0; j < verticalCuts.length; j++) {
-                int width = verticalCuts[j] - vLast;
-                maxArea = Math.max(maxArea, height * width);
-                vLast = verticalCuts[j];
-            }
-            int lastWidth = w - vLast;
-            maxArea = Math.max(maxArea, height * lastWidth);
-            if (i != horizontalCuts.length) {
-                hLast = horizontalCuts[i];
+                maxHorDiff = Math.max(maxHorDiff, horizontalCuts[i] - horizontalCuts[i - 1]);
             }
         }
-        return maxArea;
+        long maxVerDiff = 0;
+        for (int i = 0; i <= verticalCuts.length; i++) {
+            if (i == 0) {
+                maxVerDiff = Math.max(maxVerDiff, verticalCuts[i] - 0);
+            } else if (i == verticalCuts.length) {
+                maxVerDiff = Math.max(maxVerDiff, w - verticalCuts[i - 1]);
+            } else {
+                maxVerDiff = Math.max(maxVerDiff, verticalCuts[i] - verticalCuts[i - 1]);
+            }
+        }
+        return (int) (maxHorDiff % mod * maxVerDiff % mod) % mod;
     }
 
     public static void main(String[] args) {
         MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCut mapcahvc
                 = new MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCut();
-        int[] horizontalCuts = new int[]{3, 1};
-        int[] verticalCuts = new int[]{1};
+        int[] horizontalCuts = new int[]{3};
+        int[] verticalCuts = new int[]{3};
         System.out.println(mapcahvc.maxArea(5, 4, horizontalCuts, verticalCuts));
     }
 }
