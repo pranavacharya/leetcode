18fb8b6576b27c21547f735cdb16ff30bc573777
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-06 17:31:15 +0530

    min heap solution, time complexity O(NlogN)

--- src/ReductionOperationstoMaketheArrayElementsEqual.java
+++ src/ReductionOperationstoMaketheArrayElementsEqual.java
@@ -1,29 +1,21 @@
 
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class ReductionOperationstoMaketheArrayElementsEqual {
 
     public int reductionOperations(int[] nums) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> (nums[b] != nums[a]
-                ? nums[b] - nums[a] : a - b));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> (nums[a] != nums[b]
+                ? nums[a] - nums[b] : a - b));
         for (int i = 0; i < nums.length; i++) {
-            maxHeap.add(i);
+            minHeap.add(i);
         }
         int count = 0;
-        while (!maxHeap.isEmpty()) {
-            int index = maxHeap.poll();
-            ArrayList<Integer> temp = new ArrayList();
-            while (!maxHeap.isEmpty() && nums[maxHeap.peek()] == nums[index]) {
-                temp.add(maxHeap.poll());
+        while (!minHeap.isEmpty()) {
+            int min = minHeap.poll();
+            while (!minHeap.isEmpty() && nums[minHeap.peek()] == nums[min]) {
+                minHeap.poll();
             }
-            if (maxHeap.isEmpty()) {
-                return count;
-            }
-            nums[index] = nums[maxHeap.peek()];
-            maxHeap.add(index);
-            maxHeap.addAll(temp);
-            count++;
+            count += minHeap.size();
         }
         return count;
     }
