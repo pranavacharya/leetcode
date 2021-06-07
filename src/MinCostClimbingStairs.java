ce52227220c504351bc9bc7e31aee1e0035bb62a
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-07 22:46:06 +0530

    dynamic programming, top down approach

--- src/MinCostClimbingStairs.java
+++ src/MinCostClimbingStairs.java
@@ -1,21 +1,29 @@
 
+import java.util.Arrays;
 
 public class MinCostClimbingStairs {
 
-    public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length + 1];
-        dp[0] = 0;
-        dp[1] = 0;
+    private int[] dp;
 
-        for (int i = 2; i <= cost.length; i++) {
-            dp[i] = Math.min((dp[i - 1] + cost[i - 1]), (dp[i - 2] + cost[i - 2]));
+    public int minCostClimbingStairs(int[] cost) {
+        this.dp = new int[cost.length];
+        Arrays.fill(this.dp, -1);
+        return Math.min(helper(0, cost), helper(1, cost));
+    }
+
+    private int helper(int index, int[] cost) {
+        if (index >= cost.length) {
+            return 0;
         }
-        return dp[cost.length];
+        if (this.dp[index] != -1) {
+            return this.dp[index];
+        }
+        return this.dp[index] = Math.min(helper(index + 1, cost), helper(index + 2, cost)) + cost[index];
     }
 
     public static void main(String args[]) {
         MinCostClimbingStairs mccs = new MinCostClimbingStairs();
         int[] cost = new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-        System.out.print(mccs.minCostClimbingStairs(cost));
+        System.out.println(mccs.minCostClimbingStairs(cost));
     }
 }
