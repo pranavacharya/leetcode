1d2067778f670338105e6956a4d30e52664e4288
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-12 19:23:50 +0530

    dynamic programming, top down approach

--- src/StoneGameVII.java
+++ src/StoneGameVII.java
@@ -1,24 +1,33 @@
 
+import java.util.Arrays;
+
 public class StoneGameVII {
 
+    private int[][] dp;
+
     public int stoneGameVII(int[] stones) {
-        int n = stones.length;
-        int[][] dp = new int[n][n];
-
-        int[] prefix = new int[n + 1];
-
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + stones[i];
+        this.dp = new int[stones.length][stones.length];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(this.dp[i], -1);
         }
-
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                dp[i][j] = Math.max(prefix[j + 1] - prefix[i + 1] - dp[i + 1][j], prefix[j] - prefix[i] - dp[i][j - 1]);
-            }
-
+        int[] prefix = new int[stones.length + 1];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = stones[i - 1] + prefix[i - 1];
         }
-        return dp[0][n - 1];
+        return helper(stones, 0, stones.length - 1, prefix);
+    }
+
+    private int helper(int[] stones, int i, int j, int[] prefix) {
+        if (i == j) {
+            return 0;
+        }
+        if (this.dp[i][j] != -1) {
+            return this.dp[i][j];
+        }
+        int max = 0;
+        max = Math.max(prefix[j + 1] - prefix[i + 1] - helper(stones, i + 1, j, prefix),
+                prefix[j] - prefix[i] - helper(stones, i, j - 1, prefix));
+        return this.dp[i][j] = max;
     }
 
     public static void main(String args[]) {
