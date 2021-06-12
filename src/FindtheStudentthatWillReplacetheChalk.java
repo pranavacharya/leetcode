bc561c7e8f8688c23b6fa6f5b6b2bd23c7869fcf
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-12 20:43:52 +0530

    math solution, time complexity P(N)

--- src/FindtheStudentthatWillReplacetheChalk.java
+++ src/FindtheStudentthatWillReplacetheChalk.java
@@ -2,20 +2,31 @@
 public class FindtheStudentthatWillReplacetheChalk {
 
     public int chalkReplacer(int[] chalk, int k) {
-        int index = 0;
         int n = chalk.length;
-        while (true) {
-            if (chalk[index % n] > k) {
-                return index % n;
+        int[] prefix = new int[n];
+        prefix[0] = chalk[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + chalk[i];
+            if (prefix[i] > k) {
+                return i;
             }
-            k -= chalk[index % n];
-            index++;
         }
+        int sum = prefix[n - 1];
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        for (int i = 0; i < n; i++) {
+            int localMin = ((k - prefix[i]) / sum) + 1;
+            if (localMin < min) {
+                min = localMin;
+                index = i;
+            }
+        }
+        return index;
     }
 
     public static void main(String[] args) {
         FindtheStudentthatWillReplacetheChalk fsrc = new FindtheStudentthatWillReplacetheChalk();
-        int[] chalk = new int[]{3, 4, 1, 2};
-        System.out.println(fsrc.chalkReplacer(chalk, 25));
+        int[] chalk = new int[]{5, 1, 5};
+        System.out.println(fsrc.chalkReplacer(chalk, 22));
     }
 }
