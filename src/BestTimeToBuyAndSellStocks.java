1f38fb60dbdb8befd556afab0936b03c4a42dd12
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-20 02:33:13 +0530

    linear solution

--- src/BestTimeToBuyAndSellStocks.java
+++ src/BestTimeToBuyAndSellStocks.java
@@ -2,21 +2,13 @@
 public class BestTimeToBuyAndSellStocks {
 
     public int maxProfit(int[] prices) {
-        if(prices.length == 0){
-            return 0;
-        }
+        int profit = 0;
         int min = prices[0];
-        int maxprofit = 0;
-
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] - min > maxprofit) {
-                maxprofit = prices[i] - min;
-            }
-            if (prices[i] < min) {
-                min = prices[i];
-            }
+            profit = Math.max(profit, prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
-        return maxprofit;
+        return Math.max(0, profit);
     }
 
     public static void main(String args[]) {
