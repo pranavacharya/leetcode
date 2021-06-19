3bb030b5bcccd4b1e2866997764117eb083224a8
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-20 01:30:59 +0530

    two pass solution

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -2,26 +2,21 @@
 public class RotateImage {
 
     public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][n - 1 - i];
+                matrix[n - 1 - j][n - 1 - i] = temp;
+            }
+        }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i; j < matrix.length; j++) {
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
             }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix[i].length - 1 - j];
-                matrix[i][matrix[i].length - 1 - j] = temp;
-            }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.print(" " + matrix[i][j]);
-            }
-            System.out.println();
         }
     }
 
