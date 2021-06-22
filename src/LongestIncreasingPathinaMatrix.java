a7bedf20b5bb16e80201f6a8f9072a245c1fba7a
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-23 01:18:29 +0530

    dynamic programming, top down approach

--- src/LongestIncreasingPathinaMatrix.java
+++ src/LongestIncreasingPathinaMatrix.java
@@ -4,37 +4,37 @@ import java.util.Arrays;
 public class LongestIncreasingPathinaMatrix {
 
     private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    int[][] dp;
+
+    private int[][] dp;
 
     public int longestIncreasingPath(int[][] matrix) {
+        int longestPathSize = 0;
         this.dp = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        int longestPath = 0;
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                longestPath = Math.max(longestPath, dfs(matrix, i, j));
+            Arrays.fill(this.dp[i], -1);
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                longestPathSize = Math.max(longestPathSize, helper(matrix, i, j));
             }
         }
-        return longestPath;
+        return longestPathSize;
     }
 
-    private int dfs(int[][] matrix, int row, int col) {
-        if (dp[row][col] != -1) {
-            return dp[row][col];
+    private int helper(int[][] matrix, int i, int j) {
+        if (this.dp[i][j] != -1) {
+            return this.dp[i][j];
         }
-        int pathLength = 1;
-        int maxLength = 0;
+
+        int ans = 1;
         for (int[] dir : dirs) {
-            if (row + dir[0] >= 0 && row + dir[0] < matrix.length && col + dir[1] >= 0
-                    && col + dir[1] < matrix[0].length
-                    && matrix[row + dir[0]][col + dir[1]] > matrix[row][col]) {
-                maxLength = Math.max(maxLength, dfs(matrix, row + dir[0], col + dir[1]));
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (x >= 0 && x < matrix.length && y >= 0 && y < matrix[i].length && matrix[x][y] > matrix[i][j]) {
+                ans = Math.max(ans, 1 + helper(matrix, x, y));
             }
         }
-        this.dp[row][col] = pathLength + maxLength;
-        return this.dp[row][col];
+        return this.dp[i][j] = ans;
     }
 
     public static void main(String[] args) {
