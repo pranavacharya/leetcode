0c82b8b66b699fb155b72089b6423c1bdf64156a
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-28 19:38:59 +0530

    stack solution, linear solution

--- src/RemoveAllAdjacentDuplicatesInString.java
+++ src/RemoveAllAdjacentDuplicatesInString.java
@@ -3,20 +3,19 @@ import java.util.Stack;
 
 public class RemoveAllAdjacentDuplicatesInString {
 
-    public String removeDuplicates(String S) {
+    public String removeDuplicates(String s) {
         Stack<Character> stack = new Stack();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < S.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if (stack.isEmpty()) {
-                stack.push(S.charAt(i));
+                stack.push(c);
+            } else if (stack.peek() == c) {
+                stack.pop();
             } else {
-                if (stack.peek() == S.charAt(i)) {
-                    stack.pop();
-                } else {
-                    stack.push(S.charAt(i));
-                }
+                stack.push(c);
             }
         }
+        StringBuilder sb = new StringBuilder();
         while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
