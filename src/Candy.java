d6f842acca5d0cb4c436b9f461f4e895a9953b3a
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-28 19:22:26 +0530

    two pass solution, time complexity O(N), space complexity O(N)

--- src/Candy.java
+++ src/Candy.java
@@ -4,26 +4,23 @@ import java.util.Arrays;
 public class Candy {
 
     public int candy(int[] ratings) {
-        int[] pref = new int[ratings.length];
-        int[] suff = new int[ratings.length];
-        Arrays.fill(pref, 1);
-        Arrays.fill(suff, 1);
+        int candies = 0;
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                pref[i] = pref[i - 1] + 1;
+                candy[i] = candy[i - 1] + 1;
             }
         }
-
         for (int i = ratings.length - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                suff[i] = suff[i + 1] + 1;
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
             }
         }
-        int count = 0;
-        for (int i = 0; i < pref.length; i++) {
-            count += Math.max(pref[i], suff[i]);
+        for (int i = 0; i < candy.length; i++) {
+            candies += candy[i];
         }
-        return count;
+        return candies;
     }
 
     public static void main(String[] args) {
