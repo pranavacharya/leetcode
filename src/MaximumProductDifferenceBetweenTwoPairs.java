b50d8993d3a0c8ccfd0ffc39c4359e84f2b71fe0
Pranav Acharya <apranav.acharya@gmail.com>
2021-06-28 18:26:43 +0530

    linear solution

--- src/MaximumProductDifferenceBetweenTwoPairs.java
+++ src/MaximumProductDifferenceBetweenTwoPairs.java
@@ -3,14 +3,25 @@ public class MaximumProductDifferenceBetweenTwoPairs {
 
     public int maxProductDifference(int[] nums) {
         int max = 0;
+        int secondMax = 0;
         int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                max = Math.max(max, nums[i] * nums[j]);
-                min = Math.min(min, nums[i] * nums[j]);
+            if (nums[i] > max) {
+                secondMax = max;
+                max = nums[i];
+            } else if (nums[i] > secondMax) {
+                secondMax = nums[i];
+            }
+
+            if (nums[i] < min) {
+                secondMin = min;
+                min = nums[i];
+            } else if (nums[i] < secondMin) {
+                secondMin = nums[i];
             }
         }
-        return max - min;
+        return max * secondMax - min * secondMin;
     }
 
     public static void main(String[] args) {
