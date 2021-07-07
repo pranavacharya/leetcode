c6bbfe029a8542f4b3a647164b8c17038000e5a5
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-07 23:31:09 +0530

    dynamic programming, bottom up approach

--- src/DecodeWays.java
+++ src/DecodeWays.java
@@ -2,30 +2,31 @@
 public class DecodeWays {
 
     public int numDecodings(String s) {
-        int res = 0;
-        int[] mem = new int[s.length() + 1];
-        res = decode(s, s.length(), mem);
-        return res;
-    }
-
-    public int decode(String ip, int k, int[] mem) {
-        if (k == 0) {
-            return 1;
-        }
-        int l = ip.length() - k;
-        if (ip.charAt(l) == '0') {
+        if (s == null || s.length() == 0) {
             return 0;
         }
-        int ans = 0;
-        if (mem[k] != 0) {
-            return mem[k];
+
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        char[] arr = s.toCharArray();
+        if (arr[0] != '0') {
+            dp[1] = 1;
         }
-        ans = decode(ip, k - 1, mem);
-        if (k >= 2 && Integer.valueOf(ip.substring(l, l + 2)) <= 26) {
-            ans = ans + decode(ip, k - 2, mem);
+
+        for (int i = 1; i < arr.length; i++) {
+            int digit = arr[i] - '0';
+            int prev = arr[i - 1] - '0';
+            prev *= 10;
+            prev += digit;
+            if (digit >= 1 && digit <= 9) {
+                dp[i + 1] += dp[i];
+            }
+            if (prev >= 10 && prev <= 26) {
+                dp[i + 1] += dp[i - 1];
+            }
+
         }
-        mem[k] = ans;
-        return ans;
+        return dp[arr.length];
     }
 
     public static void main(String args[]) {
