9fc006db01f65ea208b226f4492ae9359be03b9c
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-08 21:11:55 +0530

    dynamic programming, bottom up approach - LCS derivative

---
+++ src/MaximumLengthofRepeatedSubarray.java
@@ -0,0 +1,24 @@
+
+public class MaximumLengthofRepeatedSubarray {
+
+    public int findLength(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+        int max = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    max = Math.max(max, dp[i + 1][j + 1]);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaximumLengthofRepeatedSubarray mlrs = new MaximumLengthofRepeatedSubarray();
+        int[] nums1 = new int[]{1, 2, 3, 2, 1};
+        int[] nums2 = new int[]{3, 2, 1, 4, 7};
+        System.out.println(mlrs.findLength(nums1, nums2));
+    }
+}
