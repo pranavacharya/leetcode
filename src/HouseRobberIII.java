5e7c0020c751d2a1dcee6d6aa147e8292e2076f5
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-08 19:44:23 +0530

    dynamic programming, top down approach

--- src/HouseRobberIII.java
+++ src/HouseRobberIII.java
@@ -2,22 +2,28 @@
 public class HouseRobberIII {
 
     public int rob(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int[] ans = dfs(root);
+        int[] ans = helper(root);
         return Math.max(ans[0], ans[1]);
     }
 
-    public int[] dfs(TreeNode node) {
-        if (node == null) {
+    private int[] helper(TreeNode root) {
+
+        if (root == null) {
             return new int[]{0, 0};
         }
-        int left[] = dfs(node.left);
-        int right[] = dfs(node.right);
-        int rob = node.val + left[1] + right[1];
-        int notRob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        return new int[]{rob, notRob};
+
+        int[] ans = new int[2];
+
+        // ans[0] = robbed
+        // ans[1] = not robbed
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+
+        ans[0] = root.val + left[1] + right[1];
+
+        ans[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        return ans;
     }
 
     public static void main(String args[]) {
