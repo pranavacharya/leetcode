f1642b1d2cfdbb60c0026155dc8aeab440c4804a
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-09 22:06:30 +0530

    dynamic programming, bottom up approach

--- src/LongestIncreasingSubsequence.java
+++ src/LongestIncreasingSubsequence.java
@@ -1,22 +1,20 @@
 
-import java.util.Arrays;
-
 public class LongestIncreasingSubsequence {
 
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
-        int len = 0;
-        for (int num : nums) {
-            int i = Arrays.binarySearch(dp, 0, len, num);
-            if (i < 0) {
-                i = -(i + 1);
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
             }
-            dp[i] = num;
-            if (i == len) {
-                len++;
-            }
+            max = Math.max(max, dp[i]);
         }
-        return len;
+        return max;
     }
 
     public static void main(String args[]) {
