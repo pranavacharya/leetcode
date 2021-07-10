877f7fb03a5fb2497a380c3c2047d3acf6d510fd
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-10 22:39:31 +0530

    brute force, time complexity O(N^3)

---
+++ src/CountSquareSumTriples.java
@@ -0,0 +1,22 @@
+
+public class CountSquareSumTriples {
+
+    public int countTriples(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                for (int k = j + 1; k <= n; k++) {
+                    if ((i * i + j * j) == k * k) {
+                        count += 2;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountSquareSumTriples csst = new CountSquareSumTriples();
+        System.out.println(csst.countTriples(10));
+    }
+}
