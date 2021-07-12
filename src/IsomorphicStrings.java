cb6c112e39c538a0d8821c70de6e01dba07a241b
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-12 21:57:32 +0530

    hashmap + hashset solution, time complexity O(N), space complexity O(N)

--- src/IsomorphicStrings.java
+++ src/IsomorphicStrings.java
@@ -12,15 +12,16 @@ public class IsomorphicStrings {
         HashSet<Character> set = new HashSet();
         for (int i = 0; i < s.length(); i++) {
             if (map.containsKey(s.charAt(i))) {
-                if (t.charAt(i) != map.get(s.charAt(i))) {
+                char mapping = map.get(s.charAt(i));
+                if (t.charAt(i) != mapping) {
                     return false;
                 }
             } else {
                 if (set.contains(t.charAt(i))) {
                     return false;
                 }
-                map.put(s.charAt(i), t.charAt(i));
                 set.add(t.charAt(i));
+                map.put(s.charAt(i), t.charAt(i));
             }
         }
         return true;
