603b45d38e9a7b8db18a88b6b7ba9db8b1fa4ceb
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-13 22:15:59 +0530

    linear solution, time complexity O(N), space complexity O(1)

--- src/FindPeakElement.java
+++ src/FindPeakElement.java
@@ -2,20 +2,21 @@
 public class FindPeakElement {
 
     public int findPeakElement(int[] nums) {
-        if (nums.length == 0 || nums.length == 1) {
-            return 0;
-        }
-        int low = 0;
-        int high = nums.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[mid + 1]) {
-                high = mid;
-            } else {
-                low = mid + 1;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                return i;
             }
         }
-        return low;
+        if (nums.length == 1) {
+            return 0;
+        }
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+        return -1;
     }
 
     public static void main(String args[]) {
