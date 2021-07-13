2371079b16afe743e92d5481bb27d13529e77a78
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-13 22:24:59 +0530

    binary search solution, time complexity O(logN), space complexity O(1)

--- src/FindPeakElement.java
+++ src/FindPeakElement.java
@@ -2,21 +2,17 @@
 public class FindPeakElement {
 
     public int findPeakElement(int[] nums) {
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                return i;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        if (nums.length == 1) {
-            return 0;
-        }
-        if (nums[0] > nums[1]) {
-            return 0;
-        }
-        if (nums[nums.length - 1] > nums[nums.length - 2]) {
-            return nums.length - 1;
-        }
-        return -1;
+        return low;
     }
 
     public static void main(String args[]) {
