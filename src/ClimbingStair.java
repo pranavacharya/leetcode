46da9d5911e1a82f72cd2d520eed5e7d3f3f2ee0
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-15 21:11:57 +0530

    dynamic programming, bottom up solution, time complexity O(N), space complexity O(N)

--- src/ClimbingStair.java
+++ src/ClimbingStair.java
@@ -2,20 +2,17 @@
 public class ClimbingStair {
 
     public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
         if (n < 3) {
-            if (n == 1) {
-                return 1;
-            } else if (n == 2) {
-                return 2;
-            } else {
-                return 0;
-            }
+            return n;
         }
         int[] dp = new int[n + 1];
-        dp[0] = 0;
+        dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        for (int i = 3; i < dp.length; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
