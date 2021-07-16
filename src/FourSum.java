c87336b1b60f275092a99a1ca7387e87c5e77e59
Pranav Acharya <apranav.acharya@gmail.com>
2021-07-16 23:14:18 +0530

    3 sum + 1, time complexity O(N^3)

--- src/FourSum.java
+++ src/FourSum.java
@@ -8,24 +8,47 @@ public class FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList();
-        helper(result, nums, 0, target, 4, new ArrayList());
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            result.addAll(threeSum(nums, i + 1, target - nums[i], nums[i]));
+        }
         return result;
     }
 
-    private void helper(List<List<Integer>> result, int[] nums, int index, int target, int k, List<Integer> path) {
-        if (target == 0 && k == 0) {
-            if (!result.contains(path)) {
-                result.add(new ArrayList(path));
+    private List<List<Integer>> threeSum(int[] nums, int index, int target, int first) {
+        List<List<Integer>> list = new ArrayList();
+        for (int k = index; k < nums.length - 2; k++) {
+            if (k != index && nums[k] == nums[k - 1]) {
+                continue;
             }
-            return;
+            int newTarget = target - nums[k];
+            int i = k + 1;
+            int j = nums.length - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] == newTarget) {
+                    List<Integer> temp = new ArrayList();
+                    temp.add(first);
+                    temp.add(nums[k]);
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    list.add(temp);
+                    do {
+                        i++;
+                    } while (i < nums.length && nums[i] == nums[i - 1]);
+                    do {
+                        j--;
+                    } while (j >= 0 && nums[j] == nums[j + 1]);
+                } else if (nums[i] + nums[j] < newTarget) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
         }
 
-        for (int i = index; i < nums.length; i++) {
-            path.add(nums[i]);
-            helper(result, nums, i + 1, target - nums[i], k - 1, path);
-            path.remove(path.size() - 1);
-            helper(result, nums, i + 1, target, k, path);
-        }
+        return list;
     }
 
     public static void main(String[] args) {
