d0d24b81ac2395ead62d8a06aeaaf961992c8b3f
Pranav Acharya <apranav.acharya@gmail.com>
2021-08-04 05:31:45 +0530

    dfs solution

--- src/PathSum2.java
+++ src/PathSum2.java
@@ -4,42 +4,51 @@ import java.util.List;
 
 public class PathSum2 {
 
-    public class TreeNode {
-
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return new ArrayList();
         }
+        List<List<Integer>> result = new ArrayList();
+        helper(result, root, targetSum, 0, new ArrayList());
+        return result;
     }
 
-    private List<List<Integer>> result = new ArrayList<>();
+    private void helper(List<List<Integer>> result, TreeNode root, int targetSum, int sum, List<Integer> path) {
+        sum += root.val;
+        path.add(root.val);
 
-    public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        if (root == null) {
-            return this.result;
-        }
-        dfs(root, new ArrayList<>(), 0, sum);
-        return this.result;
-    }
-
-    public void dfs(TreeNode root, ArrayList<Integer> tillnow, int total, int sum) {
-        if (root == null) {
+        if (root.left == null && root.right == null) {
+            if (targetSum == sum) {
+                result.add(new ArrayList(path));
+            }
+            sum -= root.val;
+            path.remove(path.size() - 1);
             return;
         }
-        total = total + root.val;
-        tillnow.add(root.val);
-        if (root.left == null && root.right == null) {
-            if (total == sum) {
-                this.result.add(tillnow);
-            }
-        } else {
-            ArrayList<Integer> leftbranch = (ArrayList<Integer>) tillnow.clone();
-            ArrayList<Integer> rightbranch = (ArrayList<Integer>) tillnow.clone();
-            dfs(root.left, leftbranch, total, sum);
-            dfs(root.right, rightbranch, total, sum);
+
+        if (root.left != null) {
+            helper(result, root.left, targetSum, sum, path);
         }
+
+        if (root.right != null) {
+            helper(result, root.right, targetSum, sum, path);
+        }
+        sum -= root.val;
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        PathSum2 ps2 = new PathSum2();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(5);
+        root.right.right.right = new TreeNode(1);
+        System.out.println(ps2.pathSum(root, 22));
     }
 }
