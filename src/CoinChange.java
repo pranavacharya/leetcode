4a568b395b0799b2ac754dd8f5620c716e9f69ba
Pranav Acharya <apranav.acharya@gmail.com>
2021-09-26 02:57:20 -0400

    dynamic programming

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 1; j <= amount; j++) {
-                if (coins[i] <= j) {
-                    dp[j] = Math.min(dp[j], 1 + dp[j - coins[i]]);
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        for (int i = 0; i < dp.length; i++){
+            Arrays.fill(dp[i] , Integer.MAX_VALUE);
+        }
+        dp[0][0] = 0;
+        for(int i = 0; i < coins.length; i++){
+            for(int j = 0; j <= amount; j++) {
+                if (coins[i] > j || dp[i + 1][j - coins[i]] == Integer.MAX_VALUE) {
+                    dp[i + 1][j] = dp[i][j];
+                } else {
+                    dp[i + 1][j] = Math.min(dp[i][j], 1 + dp[i + 1][j - coins[i]]);
                 }
             }
         }
-        return (dp[amount] > amount) ? -1 : dp[amount];
+        return dp[coins.length][amount] == Integer.MAX_VALUE ? -1 : dp[coins.length][amount];
     }
 
     public static void main(String args[]) {
