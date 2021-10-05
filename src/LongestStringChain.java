ad794fdf135a47f3283338ec62660705d51822df
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-04 22:23:31 -0400

    dynamic programming

--- src/LongestStringChain.java
+++ src/LongestStringChain.java
@@ -1,26 +1,39 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class LongestStringChain {
 
     public int longestStrChain(String[] words) {
+        int longest = 0;
+        HashSet<String> set = new HashSet();
+        for (int i = 0; i < words.length; i++) {
+            set.add(words[i]);
+        }
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
         HashMap<String, Integer> map = new HashMap();
         for (int i = 0; i < words.length; i++) {
-            map.put(words[i], 1);
-        }
-        int longest = 0;
-        Arrays.sort(words, (String s1, String s2) -> s1.length() - s2.length());
-        for (String word : words) {
-            for (int i = 0; i < word.length(); i++) {
-                String sub = word.substring(0, i) + word.substring(i + 1, word.length());
-                if (map.containsKey(sub)) {
-                    map.put(word, Math.max(map.get(word), map.get(sub) + 1));
+            map.put(words[i], map.getOrDefault(words[i], 1));
+            ArrayList<String> seq = remChar(words[i]);
+            for (int j = 0; j < seq.size(); j++) {
+                if (map.containsKey(seq.get(j))) {
+                    map.put(words[i], Math.max(map.get(words[i]), map.get(seq.get(j)) + 1));
                 }
-                longest = Math.max(longest, map.get(word));
             }
+            longest = Math.max(longest, map.get(words[i]));
         }
         return longest;
+    }
+
+    private ArrayList<String> remChar(String s) {
+        ArrayList<String> list = new ArrayList();
+        for (int i = 0; i < s.length(); i++) {
+            String temp = s.substring(0, i) + s.substring(i + 1);
+            list.add(temp);
+        }
+        return list;
     }
 
     public static void main(String[] args) {
