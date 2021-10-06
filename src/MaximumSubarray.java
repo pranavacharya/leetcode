78af458c4e9a10e0e14ab4093910837924cd5f13
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-05 22:34:01 -0400

    linear solution, time complexity O(N)

--- src/MaximumSubarray.java
+++ src/MaximumSubarray.java
@@ -2,11 +2,14 @@
 public class MaximumSubarray {
 
     public int maxSubArray(int[] nums) {
-        int cur_max = nums[0];
         int max = nums[0];
+        int sum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            cur_max = Math.max(nums[i], cur_max + nums[i]);
-            max = Math.max(max, cur_max);
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum += nums[i];
+            max = Math.max(max, sum);
         }
         return max;
     }
