e23d4553450b825479cb2d7fde8c1e5c8e3eea4b
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-05 22:26:55 -0400

    hashset solution, time complexity O(N)

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -4,10 +4,12 @@ import java.util.HashSet;
 public class ContainsDuplicate {
 
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) {
+        HashSet<Integer> set = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
                 return true;
+            } else {
+                set.add(nums[i]);
             }
         }
         return false;
