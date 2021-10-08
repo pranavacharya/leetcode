4f16c5225081e0fe71fa094f13752b926e80832f
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-08 15:19:53 -0400

    dfs

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -6,7 +6,7 @@ public class WordSearch {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0)) {
+                if (board[i][j] == word.charAt(0) && dfs(board, word, 0, i, j)) {
                     return true;
                 }
             }
@@ -14,25 +14,32 @@ public class WordSearch {
         return false;
     }
 
-    private boolean dfs(char[][] board, int i, int j, String word, int index) {
+    public boolean dfs(char[][] board, String word, int index, int i, int j) {
         if (index == word.length()) {
             return true;
         }
-        if (i >= board.length || i < 0
-                || j >= board[i].length || j < 0) {
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length) {
             return false;
         }
+
         if (word.charAt(index) != board[i][j]) {
             return false;
         }
-        char value = board[i][j];
-        board[i][j] = '#';
+
+        char c = board[i][j];
+
+        board[i][j] = '*';
+
         for (int[] dir : dirs) {
-            if (dfs(board, i + dir[0], j + dir[1], word, index + 1)) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (dfs(board, word, index + 1, x, y)) {
                 return true;
             }
         }
-        board[i][j] = value;
+
+        board[i][j] = c;
         return false;
     }
 
@@ -41,5 +48,6 @@ public class WordSearch {
         char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
         System.out.println(ws.exist(board, "ABCCED"));
         System.out.println(ws.exist(board, "FCEDASD"));
+        System.out.println(ws.exist(board, "ABCB"));
     }
 }
