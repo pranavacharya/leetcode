86a1099f6bd4ce36b96fb738f810364a91448978
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-12 22:21:23 -0400

    dynamic programming, top down approach

--- src/NthTribonacciNumber.java
+++ src/NthTribonacciNumber.java
@@ -1,24 +1,28 @@
 
+import java.util.Arrays;
+
 public class NthTribonacciNumber {
 
+    int[] memo;
+
     public int tribonacci(int n) {
-        if (n == 0) {
-            return 0;
+        this.memo = new int[n + 1];
+        Arrays.fill(this.memo, -1);
+        return helper(n);
+    }
+
+    private int helper(int n) {
+        if (this.memo[n] != -1) {
+            return this.memo[n];
         }
-        if (n == 1) {
+
+        if (n <= 1) {
+            return n;
+        } else if (n == 2) {
             return 1;
+        } else {
+            return this.memo[n] = (helper(n - 1) + helper(n - 2) + helper(n - 3));
         }
-        if (n == 2) {
-            return 1;
-        }
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i < dp.length; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        }
-        return dp[dp.length - 1];
     }
 
     public static void main(String args[]) {
