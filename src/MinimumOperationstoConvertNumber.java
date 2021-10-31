a84e50c4ee87fb3ab356401af6102ac81aecdfe1
Pranav Acharya <apranav.acharya@gmail.com>
2021-10-31 00:40:33 -0400

    bfs solution, early check

--- src/MinimumOperationstoConvertNumber.java
+++ src/MinimumOperationstoConvertNumber.java
@@ -1,5 +1,5 @@
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,52 +7,36 @@ public class MinimumOperationstoConvertNumber {
 
     public int minimumOperations(int[] nums, int start, int goal) {
         Queue<int[]> queue = new LinkedList();
-        Queue<int[]> queueEnd = new LinkedList();
         queue.add(new int[]{start, 0});
-        queueEnd.add(new int[]{goal, 0});
-
-        HashMap<Integer, Integer> visitedA = new HashMap();
-        HashMap<Integer, Integer> visitedB = new HashMap();
-
-        while (!queue.isEmpty() && !queueEnd.isEmpty()) {
+        HashSet<Integer> visited = new HashSet();
+        while (!queue.isEmpty()) {
             int[] curr = queue.poll();
-            if (visitedB.containsKey(curr[0])) {
-                return curr[1] + visitedB.get(curr[0]);
+            if (curr[0] == goal) {
+                return curr[1];
             }
-            visitedA.put(curr[0], curr[1]);
-
+            if (visited.contains(curr[0])) {
+                continue;
+            }
+            visited.add(curr[0]);
             for (int i = 0; i < nums.length; i++) {
                 int plus = curr[0] + nums[i];
                 int minus = curr[0] - nums[i];
                 int or = curr[0] ^ nums[i];
-                if (plus >= 0 && plus <= 1000) {
+
+                if (plus == goal || minus == goal || or == goal) {
+                    return curr[1] + 1;
+                }
+
+                if (plus >= 0 && plus <= 1000 && !visited.contains(plus)) {
                     queue.add(new int[]{plus, curr[1] + 1});
                 }
-                if (minus >= 0 && minus <= 1000) {
+                if (minus >= 0 && minus <= 1000 && !visited.contains(minus)) {
                     queue.add(new int[]{minus, curr[1] + 1});
                 }
-                if (or >= 0 && or <= 1000) {
+                if (or >= 0 && or <= 1000 && !visited.contains(or)) {
                     queue.add(new int[]{or, curr[1] + 1});
                 }
             }
-
-            int[] end_curr = queueEnd.poll();
-            if (visitedA.containsKey(end_curr[0])) {
-                return end_curr[1] + visitedA.get(end_curr[0]);
-            }
-            visitedB.put(end_curr[0], end_curr[1]);
-
-            for (int i = 0; i < nums.length; i++) {
-                int plus = end_curr[0] - nums[i];
-                int minus = end_curr[0] + nums[i];
-                if (plus >= 0 && plus <= 1000) {
-                    queueEnd.add(new int[]{plus, end_curr[1] + 1});
-                }
-                if (minus >= 0 && minus <= 1000) {
-                    queueEnd.add(new int[]{minus, end_curr[1] + 1});
-                }
-            }
-
         }
         return -1;
     }
