aae12b8b29109dd32a7801d4069f0439c6b96916
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-01 21:23:50 -0500

    expand from center

--- src/LongestPalindromeSubstring.java
+++ src/LongestPalindromeSubstring.java
@@ -4,24 +4,40 @@ public class LongestPalindromeSubstring {
     public String longestPalindrome(String s) {
         String ans = "";
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                if (isPalindrome(s, i, j) && ans.length() < j - i + 1) {
-                    ans = s.substring(i, j + 1);
-                }
+            String str1 = expandFromMiddle(s, i);
+            String str2 = expandFromMiddle2(s, i);
+            if (str1.length() > ans.length()) {
+                ans = str1;
+            }
+
+            if (str2.length() > ans.length()) {
+                ans = str2;
             }
         }
         return ans;
     }
 
-    private boolean isPalindrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
+    private String expandFromMiddle(String s, int index) {
+        int left = index;
+        int right = index;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return true;
+        return s.substring(left + 1, right);
+    }
+
+    private String expandFromMiddle2(String s, int index) {
+        int left = index;
+        int right = index + 1;
+        if (left < 0 || right >= s.length() || s.charAt(left) != s.charAt(right)) {
+            return "";
+        }
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
     }
 
     public static void main(String args[]) {
