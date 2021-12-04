936a9d454bea1d5ca336134c566c400016c17c77
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-04 17:07:21 -0500

    linear solution

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -2,21 +2,14 @@
 public class JumpGame {
 
     public boolean canJump(int[] nums) {
-        boolean[] dp = new boolean[nums.length];
-        if (nums[0] < 1 && nums.length > 1) {
-            return false;
-        }
-        dp[0] = true;
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = false;
-            for (int j = i - 1; j >= 0; j--) {
-                if (dp[j] && nums[j] >= (i - j)) {
-                    dp[i] = true;
-                    break;
-                }
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < i) {
+                return false;
             }
+            max = Math.max(max, nums[i] + i);
         }
-        return dp[nums.length - 1];
+        return max >= nums.length - 1;
     }
 
     public static void main(String args[]) {
