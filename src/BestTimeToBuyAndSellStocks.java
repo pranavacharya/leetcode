0b7648e1e46e3bd98038e0f855671ceba75c19b5
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-07 18:31:27 -0500

    linear solution, time complexity O(N)

--- src/BestTimeToBuyAndSellStocks.java
+++ src/BestTimeToBuyAndSellStocks.java
@@ -2,13 +2,13 @@
 public class BestTimeToBuyAndSellStocks {
 
     public int maxProfit(int[] prices) {
-        int profit = 0;
         int min = prices[0];
+        int profit = 0;
         for (int i = 1; i < prices.length; i++) {
-            profit = Math.max(profit, prices[i] - min);
             min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
         }
-        return Math.max(0, profit);
+        return profit;
     }
 
     public static void main(String args[]) {
