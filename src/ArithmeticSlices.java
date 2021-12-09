27f0fc1973bcd2112cf81af6b7dc39aa23d12c44
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-09 16:44:02 -0500

    dynamic programming, top down approach, time complexity O(N^2), TLE

--- src/ArithmeticSlices.java
+++ src/ArithmeticSlices.java
@@ -1,26 +1,50 @@
 
+import java.util.Arrays;
+
 public class ArithmeticSlices {
 
-    public int numberOfArithmeticSlices(int[] A) {
-        int i = 0;
-        int j = i + 1;
-        int ans = 0;
-        while (j < A.length) {
-            int diff = A[j] - A[i];
-            while (j + 1 < A.length && A[j + 1] - A[j] == diff) {
-                j++;
-            }
+    private int[][] dp;
 
-            int count = j - i;
-            if (count >= 2) {
-                int local = (count * (count - 1)) / 2;
-                ans += local;
-            }
-            i = j;
-            j = i + 1;
+    public int numberOfArithmeticSlices(int[] nums) {
+        this.dp = new int[nums.length][nums.length];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(this.dp[i], -1);
+        }
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private int helper(int[] nums, int i, int j) {
+
+        if (this.dp[i][j] != -1) {
+            return 0; // not using previously counted
         }
 
-        return ans;
+        if (j - i + 1 < 3) {
+            return this.dp[i][j] = 0;
+        }
+
+        boolean valid = true;
+
+        for (int start = i + 1; start < j; start++) {
+            if (nums[start] - nums[start - 1] != nums[start + 1] - nums[start]) {
+                valid = false;
+                break;
+            }
+        }
+
+        int count = 0;
+
+        for (int k = i; k < j; k++) {
+            count += (helper(nums, i, k));
+            count += (helper(nums, k + 1, j));
+        }
+
+        if (valid) {
+            count++;
+        }
+
+        return this.dp[i][j] = count;
+
     }
 
     public static void main(String args[]) {
