8470d519c6374d975021c3e163441ca936767218
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-11 23:38:24 -0500

    dynamic programming, bottom up approach, time complexity O(N)

--- src/DecodeWays.java
+++ src/DecodeWays.java
@@ -2,31 +2,23 @@
 public class DecodeWays {
 
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-
         int[] dp = new int[s.length() + 1];
         dp[0] = 1;
-        char[] arr = s.toCharArray();
-        if (arr[0] != '0') {
-            dp[1] = 1;
-        }
-
-        for (int i = 1; i < arr.length; i++) {
-            int digit = arr[i] - '0';
-            int prev = arr[i - 1] - '0';
-            prev *= 10;
-            prev += digit;
-            if (digit >= 1 && digit <= 9) {
-                dp[i + 1] += dp[i];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                dp[i + 1] = 0;
+            } else {
+                dp[i + 1] = dp[i];
             }
-            if (prev >= 10 && prev <= 26) {
+            if (i > 0 && s.charAt(i) >= '0' && s.charAt(i) <= '6'
+                    && s.charAt(i - 1) == '2') {
+                dp[i + 1] += dp[i - 1];
+            } else if (i > 0 && s.charAt(i) >= '0' && s.charAt(i) <= '9'
+                    && s.charAt(i - 1) == '1') {
                 dp[i + 1] += dp[i - 1];
             }
-
         }
-        return dp[arr.length];
+        return dp[s.length()];
     }
 
     public static void main(String args[]) {
