075888a928e30c4f1721795d60fe4f1c0df51403
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-14 00:34:34 -0500

    dfs + trie solution

--- src/WordSearchII.java
+++ src/WordSearchII.java
@@ -6,57 +6,73 @@ public class WordSearchII {
 
     private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    public List<String> findWords(char[][] board, String[] words) {
-        List<String> ans = new ArrayList();
+    class Trie {
 
-        for (String word : words) {
-            outer:
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if (word.charAt(0) == board[i][j]) {
-                        if (findWord(board, i, j, word, 0)) {
-                            ans.add(word);
-                            break outer;
-                        }
-                    }
-                }
-            }
-        }
-
-        return ans;
+        Trie[] children = new Trie[26];
+        boolean isEnd = false;
+        String word;
     }
 
-    private boolean findWord(char[][] board, int i, int j, String word, int index) {
-        if (index == word.length() || index == 0 && word.length() == 1) {
-            return true;
+    private void addWord(Trie root, String word) {
+        Trie temp = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (temp.children[c - 'a'] == null) {
+                temp.children[c - 'a'] = new Trie();
+            }
+            temp = temp.children[c - 'a'];
         }
+        temp.isEnd = true;
+        temp.word = word;
+    }
 
-        if (board[i][j] != word.charAt(index)) {
-            return false;
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie root = new Trie();
+        for (int i = 0; i < words.length; i++) {
+            addWord(root, words[i]);
         }
-
-        char c = board[i][j];
-        board[i][j] = '.';
-        for (int[] dir : dirs) {
-            int x = i + dir[0];
-            int y = j + dir[1];
-            if (x >= 0 && x < board.length && y >= 0 && y < board[x].length) {
-                if (findWord(board, x, y, word, index + 1)) {
-                    board[i][j] = c;
-                    return true;
-                }
+        List<String> result = new ArrayList();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                dfs(board, root, result, i, j);
             }
         }
+        return result;
+    }
+
+    private void dfs(char[][] board, Trie root, List<String> list, int i, int j) {
+        if (root == null) {
+            return;
+        }
+        char c = board[i][j];
+
+        if (board[i][j] == '#' || root.children[c - 'a'] == null) {
+            return;
+        }
+        root = root.children[c - 'a'];
+        if (root != null && root.isEnd) {
+            list.add(root.word);
+            root.isEnd = false;
+        }
+        board[i][j] = '#';
+
+        for (int[] dir : dirs) {
+            int x = dir[0] + i;
+            int y = dir[1] + j;
+            if (x >= 0 && x < board.length && y >= 0 && y < board[x].length) {
+                dfs(board, root, list, x, y);
+            }
+        }
+
         board[i][j] = c;
-        return false;
     }
 
     public static void main(String[] args) {
         WordSearchII ws2 = new WordSearchII();
-//        char[][] board = new char[][]{{'a', 'a'}};
-//        String[] words = new String[]{"aaa"};
-        char[][] board = new char[][]{{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
-        String[] words = new String[]{"oath", "pea", "eat", "rain"};
+        char[][] board = new char[][]{{'a', 'a'}};
+        String[] words = new String[]{"aaa"};
+//        char[][] board = new char[][]{{'o', 'a', 'a', 'n'}, {'e', 't', 'a', 'e'}, {'i', 'h', 'k', 'r'}, {'i', 'f', 'l', 'v'}};
+//        String[] words = new String[]{"oath", "pea", "eat", "rain"};
         System.out.println(ws2.findWords(board, words));
     }
 }
