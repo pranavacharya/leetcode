6ec145db37b8e826ca0e11813825d11da6e89e8e
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-23 02:42:41 -0500

    dynamic programming solution, linear solution - contribution

--- src/CountUniqueCharactersofAllSubstringsofaGivenString.java
+++ src/CountUniqueCharactersofAllSubstringsofaGivenString.java
@@ -1,44 +1,24 @@
 
 public class CountUniqueCharactersofAllSubstringsofaGivenString {
 
-    boolean dp[][];
-
     public int uniqueLetterString(String s) {
-        int[][] freq = new int[s.length() + 1][26];
+        int ans = 0;
+        int[] last = new int[26];
+        int[] dp = new int[26];
+        int curr = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            for (int k = 0; k < 26; k++) {
-                freq[i + 1][k] = freq[i][k];
-            }
-            freq[i + 1][c - 'A'] = freq[i][c - 'A'] + 1;
+            curr -= dp[c - 'A'];
+            dp[c - 'A'] = i - (last[c - 'A'] - 1);
+            curr += dp[c - 'A'];
+            last[c - 'A'] = i + 1;
+            ans += curr;
         }
-        this.dp = new boolean[s.length() + 1][s.length() + 1];
-        // System.out.println(Arrays.deepToString(freq));
-        return helper(s, 0, s.length() - 1, freq);
-    }
-
-    private int helper(String s, int i, int j, int[][] freq) {
-        if (dp[i][j]) {
-            return 0;
-        }
-        int sum = 0;
-
-        for (int k = 0; k < 26; k++) {
-            if (freq[j + 1][k] - freq[i][k] == 1) {
-                sum++;
-            }
-        }
-
-        for (int k = i; k < j; k++) {
-            sum += helper(s, i, k, freq);
-            sum += helper(s, k + 1, j, freq);
-        }
-        dp[i][j] = true;
-        return sum;
+        return ans;
     }
 
     public static void main(String[] args) {
         CountUniqueCharactersofAllSubstringsofaGivenString cucasgs = new CountUniqueCharactersofAllSubstringsofaGivenString();
-        System.out.println(cucasgs.uniqueLetterString("ABC"));
+        System.out.println(cucasgs.uniqueLetterString("LEETCODE"));
     }
 }
