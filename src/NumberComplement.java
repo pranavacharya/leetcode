0129e7dab97cdca6df9365defbb3fa3622f518c9
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-27 18:03:26 -0500

    bits mask solution

--- src/NumberComplement.java
+++ src/NumberComplement.java
@@ -2,16 +2,19 @@
 public class NumberComplement {
 
     public int findComplement(int num) {
-        int x = (int) (Math.log(num)
+        int mask = 1;
+        int bits = (int) (Math.log(num)
                 / Math.log(2)) + 1;
-        for (int i = 0; i < x; i++) {
-            num = (num ^ (1 << i));
+        while (bits > 0) {
+            num = num ^ mask;
+            mask = mask << 1;
+            bits--;
         }
         return num;
     }
 
     public static void main(String args[]) {
         NumberComplement nc = new NumberComplement();
-        System.out.println(nc.findComplement(5));
+        System.out.println(nc.findComplement(2147483647));
     }
 }
