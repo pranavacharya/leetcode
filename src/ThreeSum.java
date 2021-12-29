dd0378cec478eaadb39d494c9f7b3c024168092d
Pranav Acharya <apranav.acharya@gmail.com>
2021-12-28 21:05:25 -0500

    sort and two pointer solution, time complexity O(N^2)

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -6,29 +6,44 @@ import java.util.List;
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList();
+        if (nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+            int first = nums[i];
             int j = i + 1;
             int k = nums.length - 1;
             while (j < k) {
-                if (nums[i] + nums[j] + nums[k] == 0) {
+                if (nums[j] + nums[k] + first == 0) {
                     List<Integer> temp = new ArrayList();
-                    temp.add(nums[i]);
+                    temp.add(first);
                     temp.add(nums[j]);
                     temp.add(nums[k]);
+                    // if(!result.contains(temp))
                     result.add(temp);
-                    j++;
-                    while (j < k && nums[j] == nums[j - 1]) {
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+                    k--;
+                    while (j < k && nums[j] == nums[j + 1]) {
                         j++;
                     }
-                } else if (nums[i] + nums[j] + nums[k] < 0) {
                     j++;
-                } else {
+                } else if (nums[j] + nums[k] + first > 0) {
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
                     k--;
+                } else {
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+                    j++;
                 }
             }
         }
