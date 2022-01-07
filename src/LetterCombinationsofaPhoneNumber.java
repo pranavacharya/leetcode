29b572e1f7a74f1451705229959df358c5ace666
Pranav Acharya <apranav.acharya@gmail.com>
2022-01-07 16:20:27 -0500

    dfs solution

--- src/LetterCombinationsofaPhoneNumber.java
+++ src/LetterCombinationsofaPhoneNumber.java
@@ -1,77 +1,37 @@
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class LetterCombinationsofaPhoneNumber {
 
-    private HashMap<Character, ArrayList<Character>> map;
-
-    LetterCombinationsofaPhoneNumber() {
-        this.map = new HashMap();
-        ArrayList<Character> two = new ArrayList();
-        two.add('a');
-        two.add('b');
-        two.add('c');
-        ArrayList<Character> three = new ArrayList();
-        three.add('d');
-        three.add('e');
-        three.add('f');
-        ArrayList<Character> four = new ArrayList();
-        four.add('g');
-        four.add('h');
-        four.add('i');
-        ArrayList<Character> five = new ArrayList();
-        five.add('j');
-        five.add('k');
-        five.add('l');
-        ArrayList<Character> six = new ArrayList();
-        six.add('m');
-        six.add('n');
-        six.add('o');
-        ArrayList<Character> seven = new ArrayList();
-        seven.add('p');
-        seven.add('q');
-        seven.add('r');
-        seven.add('s');
-        ArrayList<Character> eight = new ArrayList();
-        eight.add('t');
-        eight.add('u');
-        eight.add('v');
-        ArrayList<Character> nine = new ArrayList();
-        nine.add('w');
-        nine.add('x');
-        nine.add('y');
-        nine.add('z');
-        this.map.put('2', two);
-        this.map.put('3', three);
-        this.map.put('4', four);
-        this.map.put('5', five);
-        this.map.put('6', six);
-        this.map.put('7', seven);
-        this.map.put('8', eight);
-        this.map.put('9', nine);
-    }
+    private char[][] map;
 
     public List<String> letterCombinations(String digits) {
+        this.map = new char[10][];
+        map[2] = new char[]{'a', 'b', 'c'};
+        map[3] = new char[]{'d', 'e', 'f'};
+        map[4] = new char[]{'g', 'h', 'i'};
+        map[5] = new char[]{'j', 'k', 'l'};
+        map[6] = new char[]{'m', 'n', 'o'};
+        map[7] = new char[]{'p', 'q', 'r', 's'};
+        map[8] = new char[]{'t', 'u', 'v'};
+        map[9] = new char[]{'w', 'x', 'y', 'z'};
         List<String> result = new ArrayList();
-        if (digits.length() == 0) {
-            return result;
-        }
-        dfs(digits, 0, result, new StringBuilder());
+        helper(result, 0, digits, new StringBuilder());
         return result;
     }
 
-    private void dfs(String s, int index, List<String> result, StringBuilder path) {
-        if (index == s.length() && path.length() == s.length()) {
-            result.add(path.toString());
+    private void helper(List<String> result, int index, String digits, StringBuilder path) {
+        if (index == digits.length()) {
+            if (path.length() != 0) {
+                result.add(path.toString());
+            }
             return;
         }
-        ArrayList<Character> alpha = this.map.get(s.charAt(index));
-        for (int i = 0; i < alpha.size(); i++) {
-            char c = alpha.get(i);
+
+        for (char c : map[digits.charAt(index) - '0']) {
             path.append(c);
-            dfs(s, index + 1, result, path);
+            helper(result, index + 1, digits, path);
             path.deleteCharAt(path.length() - 1);
         }
     }
