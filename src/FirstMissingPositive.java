44ad8b7677b4b7ac141652f7d10efb7180a6bd7c
Pranav Acharya <apranav.acharya@gmail.com>
2022-01-07 23:35:38 -0500

    linear solution

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -2,24 +2,26 @@
 public class FirstMissingPositive {
 
     public int firstMissingPositive(int[] nums) {
+        int non = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > nums.length || nums[i] <= 0) {
+            if (nums[i] <= 0) {
+                non++;
+            }
+        }
+        int valid = nums.length - non;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0) {
                 nums[i] = nums.length + 1;
             }
         }
-
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < nums.length + 1) {
-                int index = Math.abs(nums[i]) - 1;
-                if (index >= nums.length) {
-                    continue;
-                }
-                if (nums[index] > 0) {
-                    nums[index] = -1 * nums[index];
+            if (Math.abs(nums[i]) <= valid) {
+                int val = Math.abs(nums[i]);
+                if (nums[val - 1] > 0) {
+                    nums[val - 1] = nums[val - 1] * -1;
                 }
             }
         }
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
                 return i + 1;
