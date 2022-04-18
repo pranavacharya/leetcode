fb3fd59cfaabc6e97edcd5d2b4f9d33171ad6857
Pranav Acharya <apranav.acharya@gmail.com>
2022-04-18 02:28:01 -0400

    inorder traversal, dfs solution

--- src/KthSmallestElementinaBST.java
+++ src/KthSmallestElementinaBST.java
@@ -1,32 +1,43 @@
 
 public class KthSmallestElementinaBST {
 
-    private int count;
-    private int ans;
-    private boolean found;
-
-    public KthSmallestElementinaBST() {
-        this.count = 0;
-        this.found = false;
-        this.ans = -1;
-    }
+    int count;
+    int k;
+    int ans;
 
     public int kthSmallest(TreeNode root, int k) {
-        this.count = k;
-        inOrderTraversal(root);
+        this.k = k;
+        this.count = 0;
+        this.ans = root.val;
+
+        inorder(root);
+
         return this.ans;
     }
 
-    public void inOrderTraversal(TreeNode root) {
-        if (root != null && !this.found) {
-            inOrderTraversal(root.left);
-            this.count--;
-            if (this.count == 0) {
-                this.ans = root.val;
-                this.found = true;
-            }
-            inOrderTraversal(root.right);
+    private void inorder(TreeNode root) {
+        if (root == null) {
+            return;
         }
+
+        inorder(root.left);
+        this.count++;
+        if (this.k == this.count) {
+            ans = root.val;
+        }
+        inorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        KthSmallestElementinaBST kme = new KthSmallestElementinaBST();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(1);
+        System.out.println(kme.kthSmallest(root, 3));
+        
     }
 
 }
