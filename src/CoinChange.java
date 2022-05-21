9dd36400d2981b98756858657afbcbe2a3e74e29
Pranav Acharya <apranav.acharya@gmail.com>
2022-05-20 21:18:18 -0400

    dynamic programming, bottom up approach

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -4,21 +4,28 @@ import java.util.Arrays;
 public class CoinChange {
 
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int i = 0; i < dp.length; i++){
-            Arrays.fill(dp[i] , Integer.MAX_VALUE);
-        }
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        Arrays.fill(dp[0], Integer.MAX_VALUE);
         dp[0][0] = 0;
-        for(int i = 0; i < coins.length; i++){
-            for(int j = 0; j <= amount; j++) {
-                if (coins[i] > j || dp[i + 1][j - coins[i]] == Integer.MAX_VALUE) {
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) {
+                    dp[i + 1][j] = 0;
+                    continue;
+                }
+                if (j < coins[i]) {
                     dp[i + 1][j] = dp[i][j];
                 } else {
-                    dp[i + 1][j] = Math.min(dp[i][j], 1 + dp[i + 1][j - coins[i]]);
-                }
+                    if (dp[i + 1][j - coins[i]] == Integer.MAX_VALUE) {
+                        dp[i + 1][j] = dp[i][j];        
+                    } else {
+                        dp[i + 1][j] = Math.min(dp[i][j], 1 + dp[i + 1][j - coins[i]]);    
+                    }
+                }                    
             }
-        }
-        return dp[coins.length][amount] == Integer.MAX_VALUE ? -1 : dp[coins.length][amount];
+        }  
+        return dp[n][amount] == Integer.MAX_VALUE ? -1 : dp[n][amount];
     }
 
     public static void main(String args[]) {
