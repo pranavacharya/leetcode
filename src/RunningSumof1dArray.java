1a9d27069ea96762b19c356c6de191b279d05cf2
Pranav Acharya <apranav.acharya@gmail.com>
2022-06-01 02:17:19 -0400

    linear solution

--- src/RunningSumof1dArray.java
+++ src/RunningSumof1dArray.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 public class RunningSumof1dArray {
 
     public int[] runningSum(int[] nums) {
-        int[] ans = new int[nums.length];
-        ans[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            ans[i] = ans[i - 1] + nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                continue;
+            }
+            nums[i] += nums[i - 1];
         }
-        return ans;
+        return nums;
     }
 
     public static void main(String args[]) {
