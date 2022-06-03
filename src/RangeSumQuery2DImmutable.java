3e8d36044e3a37d1d6a9fdf7218e9882e81075d6
Pranav Acharya <apranav.acharya@gmail.com>
2022-06-02 21:26:06 -0400

    dynamic programming solution, bottom up approach

--- src/RangeSumQuery2DImmutable.java
+++ src/RangeSumQuery2DImmutable.java
@@ -1,30 +1,46 @@
 
+import java.util.Arrays;
+
 public class RangeSumQuery2DImmutable {
 
     private int[][] dp;
 
     public RangeSumQuery2DImmutable(int[][] matrix) {
-        if (matrix.length != 0) {
-            this.dp = new int[matrix.length][matrix[0].length];
-            for (int i = 0; i < dp.length; i++) {
-                for (int j = 0; j < dp[0].length; j++) {
-                    if (j == 0) {
-                        dp[i][j] = matrix[i][j];
-                    } else {
-                        dp[i][j] = dp[i][j - 1] + matrix[i][j];
-                    }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        this.dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = matrix[i][j];
+                    continue;
                 }
+                if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + matrix[i][j];
+                    continue;
+                }
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + matrix[i][j];
+                    continue;
+                }
+                dp[i][j] = matrix[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
             }
         }
+        System.out.println(Arrays.deepToString(this.dp));
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for (int i = row1; i <= row2; i++) {
-            int min = (col1 - 1) < 0 ? 0 : dp[i][col1 - 1];
-            sum += (dp[i][col2] - min);
+        int ans = dp[row2][col2];
+        if (row1 > 0 && col1 > 0) {
+            ans += dp[row1 - 1][col1 - 1];
         }
-        return sum;
+        if (row1 > 0) {
+            ans -= dp[row1 - 1][col2];
+        }
+        if (col1 > 0) {
+            ans -= dp[row2][col1 - 1];
+        }
+        return ans;
     }
 
     public static void main(String args[]) {
